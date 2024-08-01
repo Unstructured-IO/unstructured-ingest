98fd8618ebef594bf33a261f8ce83d6ef23f7a11
Roman Isecke <roman@unstructured.io>
2024-07-31 22:03:16 -0400

    Fix docker compose command

--- test_e2e/dest/kafka-local.sh
+++ test_e2e/dest/kafka-local.sh
@@ -25,7 +25,7 @@ function cleanup {
   cleanup_dir "$OUTPUT_DIR"
 
   echo "Stopping local Kafka instance"
-  docker compose-f "$SCRIPT_DIR"/env_setup/kafka/docker-compose.yml down --remove-orphans -v
+  docker compose -f "$SCRIPT_DIR"/env_setup/kafka/docker-compose.yml down --remove-orphans -v
 }
 
 trap cleanup EXIT
