3960aacb672f7e359372a135a214717e17610305
Roman Isecke <roman@unstructured.io>
2024-08-01 09:23:28 -0400

    lint shell

--- scripts/install-docker-compose.sh
+++ scripts/install-docker-compose.sh
@@ -13,11 +13,11 @@ DOCKER_VERSION=26.1.3
 BUILDX_VERSION=0.16.0
 DOCKER_COMPOSE_VERSION=2.28.1
 
-curl -fLo docker.tgz https://download.docker.com/${TARGETOS}/static/stable/${DOCKER_ARCH}/docker-${DOCKER_VERSION}.tgz
+curl -fLo docker.tgz https://download.docker.com/${TARGETOS}/static/stable/"${DOCKER_ARCH}"/docker-${DOCKER_VERSION}.tgz
 tar zxvf docker.tgz
 rm -rf docker.tgz
 mkdir -p /usr/local/lib/docker/cli-plugins
 curl -fLo /usr/local/lib/docker/cli-plugins/docker-buildx "https://github.com/docker/buildx/releases/download/v${BUILDX_VERSION}/buildx-v${BUILDX_VERSION}.linux-${TARGETARCH}"
 chmod +x /usr/local/lib/docker/cli-plugins/docker-buildx
-curl -SL https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-${TARGETOS}-${DOCKER_ARCH} -o /usr/local/lib/docker/cli-plugins/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-${TARGETOS}-"${DOCKER_ARCH}" -o /usr/local/lib/docker/cli-plugins/docker-compose
 chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
