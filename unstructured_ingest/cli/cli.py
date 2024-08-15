70cbc4477a68aef3dc236a2abf10f65b8445e2c6
Roman Isecke <roman@unstructured.io>
2024-08-15 15:38:40 -0400

    Make httpx a connector-specific dep

--- unstructured_ingest/cli/cli.py
+++ unstructured_ingest/cli/cli.py
@@ -1,8 +1,14 @@
+from typing import TYPE_CHECKING
+
 import click
 
 from unstructured_ingest.cli import dest, src
+from unstructured_ingest.utils.dep_check import requires_dependencies
 from unstructured_ingest.v2.cli.cmds import dest as dest_v2
 from unstructured_ingest.v2.cli.cmds import src as src_v2
+
+if TYPE_CHECKING:
+    from click import Command
 
 
 @click.group()
@@ -10,7 +16,8 @@ def ingest():
     pass
 
 
-def get_cmd() -> click.Command:
+@requires_dependencies(dependencies=["click"], extras="cli")
+def get_cmd() -> "Command":
     """Construct and return a Click command object representing the main command for the CLI.
 
     This function adds all dest_subcommand(s) to each src_subcommand, and adds all of those
