0dbc8f3ed29d2a70d8c1635633251e2194e10946
Roman Isecke <roman@unstructured.io>
2024-08-15 15:12:21 -0400

    Isolate backoff dep

--- unstructured_ingest/pipeline/interfaces.py
+++ unstructured_ingest/pipeline/interfaces.py
@@ -11,7 +11,6 @@ from pathlib import Path
 from dataclasses_json import DataClassJsonMixin
 
 from unstructured_ingest.error import SourceConnectionNetworkError
-from unstructured_ingest.ingest_backoff import RetryHandler
 from unstructured_ingest.interfaces import (
     BaseDestinationConnector,
     BaseSourceConnector,
@@ -21,6 +20,9 @@ from unstructured_ingest.interfaces import (
     RetryStrategyConfig,
 )
 from unstructured_ingest.logger import ingest_log_streaming_init, logger
+
+if t.TYPE_CHECKING:
+    from unstructured_ingest.ingest_backoff import RetryHandler
 
 
 @dataclass
@@ -146,9 +148,11 @@ class SourceNode(PipelineNode):
     retry_strategy_config: t.Optional[RetryStrategyConfig] = None
 
     @property
-    def retry_strategy(self) -> t.Optional[RetryHandler]:
+    def retry_strategy(self) -> t.Optional["RetryHandler"]:
         if retry_strategy_config := self.retry_strategy_config:
             import backoff
+
+            from unstructured_ingest.ingest_backoff import RetryHandler
 
             return RetryHandler(
                 backoff.expo,
