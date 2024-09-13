5f2ed5ac66233f2ae2dec5447ae4dd94f2510c83
Roman Isecke <roman@unstructured.io>
2024-09-13 09:45:54 -0400

    tidy shell

--- test_e2e/src/local-single-file-with-pdf-infer-table-structure.sh
+++ test_e2e/src/local-single-file-with-pdf-infer-table-structure.sh
@@ -36,4 +36,4 @@ PYTHONPATH=${PYTHONPATH:-.} "$RUN_SCRIPT" \
   --input-path "$SCRIPT_DIR"/example-docs/ \
   --work-dir "$WORK_DIR"
 
-"$SCRIPT_DIR"/check-diff-expected-output.py --output-folder-name $OUTPUT_FOLDER_NAMEE
+"$SCRIPT_DIR"/check-diff-expected-output.py --output-folder-name "$OUTPUT_FOLDER_NAME"
