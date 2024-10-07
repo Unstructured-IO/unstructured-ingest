0e181630619c16194b6348198d5b52904acac894
Yao You <yao@unstructured.io>
2024-10-07 16:47:33 -0500

    pass in str of tuple into field constructor

--- unstructured_ingest/v2/processes/connectors/pinecone.py
+++ unstructured_ingest/v2/processes/connectors/pinecone.py
@@ -78,7 +78,7 @@ ALLOWED_FIELDS = (
 
 class PineconeUploadStagerConfig(UploadStagerConfig):
     metadata_fields: list[str] = Field(
-        default=ALLOWED_FIELDS,
+        default=str(ALLOWED_FIELDS),
         description=(
             "which metadata from the source element to map to the payload metadata being sent to "
             "Pinecone."
