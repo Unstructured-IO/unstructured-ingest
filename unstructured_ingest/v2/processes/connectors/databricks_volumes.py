276e12913019f4e5c1640836becf473c53194ca4
hubert.rutkowski <hubert.rutkowski@deepsense.ai>
2024-10-09 17:23:31 +0200

    Move in Romans's code from branch feat/databricks-volumes-src

--- unstructured_ingest/v2/processes/connectors/databricks_volumes.py
+++ unstructured_ingest/v2/processes/connectors/databricks_volumes.py
@@ -1,11 +1,12 @@
 import os
 from dataclasses import dataclass
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, Optional
+from typing import TYPE_CHECKING, Any, Optional, Generator
 
 from pydantic import Field, Secret
 
-from unstructured_ingest.error import DestinationConnectionError
+from unstructured.documents.elements import DataSourceMetadata
+from unstructured_ingest.error import DestinationConnectionError, SourceConnectionNetworkError
 from unstructured_ingest.utils.dep_check import requires_dependencies
 from unstructured_ingest.v2.interfaces import (
     AccessConfig,
@@ -13,9 +14,12 @@ from unstructured_ingest.v2.interfaces import (
     FileData,
     Uploader,
     UploaderConfig,
+    Indexer,
+    IndexerConfig,
+    SourceIdentifiers, Downloader, DownloaderConfig
 )
 from unstructured_ingest.v2.logger import logger
-from unstructured_ingest.v2.processes.connector_registry import DestinationRegistryEntry
+from unstructured_ingest.v2.processes.connector_registry import DestinationRegistryEntry, SourceRegistryEntry
 
 if TYPE_CHECKING:
     from databricks.sdk import WorkspaceClient
@@ -95,6 +99,29 @@ class DatabricksVolumesConnectionConfig(ConnectionConfig):
     )
 
 
+@dataclass
+class DatabricksVolumesIndexerConfig(IndexerConfig):
+    remote_url: str
+    recursive: bool = False
+    catalog: str = Field(init=False)
+    path: str = Field(init=False)
+    full_name: str = Field(init=False)
+
+    def __post_init__(self):
+        full_path = self.remote_url
+
+        if full_path.startswith("/"):
+            full_path = full_path[1:]
+        parts = full_path.split("/")
+        if parts[0] != "Volumes":
+            raise ValueError(
+                "remote url needs to be of the format /Volumes/catalog_name/volume/path"
+            )
+        self.catalog = parts[1]
+        self.path = "/".join(parts[2:])
+        self.full_name = ".".join(parts[1:])
+
+
 class DatabricksVolumesUploaderConfig(UploaderConfig):
     volume: str = Field(description="Name of volume in the Unity Catalog")
     catalog: str = Field(description="Name of the catalog in the Databricks Unity Catalog service")
@@ -116,6 +143,93 @@ class DatabricksVolumesUploaderConfig(UploaderConfig):
         if self.volume_path:
             path = f"{path}/{self.volume_path}"
         return path
+
+
+
+@dataclass
+class DatabricksVolumesIndexer(Indexer):
+    connector_type: str = CONNECTOR_TYPE
+    index_config: DatabricksVolumesIndexerConfig
+    connection_config: DatabricksVolumesConnectionConfig
+    workspace: "WorkspaceClient" = Field(init=False)
+
+    def __post_init__(self):
+        self.workspace = self.connection_config.get_client()
+
+    def run(self, **kwargs: Any) -> Generator[FileData, None, None]:
+        for file_info in self.workspace.dbfs.list(
+                path=self.index_config.remote_url, recursive=self.index_config.recursive
+        ):
+            if file_info.is_dir:
+                continue
+            rel_path = file_info.path.replace(self.index_config.remote_url, "")
+            if rel_path.startswith("/"):
+                rel_path = rel_path[1:]
+            filename = Path(file_info.path).name
+            yield FileData(
+                identifier=file_info.path,
+                connector_type=CONNECTOR_TYPE,
+                source_identifiers=SourceIdentifiers(
+                    filename=filename,
+                    rel_path=rel_path,
+                    fullpath=file_info.path,
+                    additional_metadata={
+                        "catalog": self.index_config.catalog,
+                    },
+                ),
+                metadata=DataSourceMetadata(
+                    url=file_info.path, date_modified=str(file_info.modification_time)
+                ),
+            )
+
+
+@dataclass
+class DatabricksVolumesDownloaderConfig(DownloaderConfig):
+    pass
+
+
+@dataclass
+class DatabricksVolumesDownloader(Downloader):
+    download_config: DatabricksVolumesDownloaderConfig
+    connection_config: DatabricksVolumesConnectionConfig
+    workspace: "WorkspaceClient" = Field(init=False)
+
+    def __post_init__(self):
+        self.workspace = self.connection_config.get_client()
+
+    def get_download_path(self, file_data: FileData) -> Path:
+        return self.download_config.download_dir / Path(file_data.source_identifiers.relative_path)
+
+    @staticmethod
+    def is_float(value: str):
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
+
+    def run(self, file_data: FileData, **kwargs: Any) -> Path:
+        download_path = self.get_download_path(file_data=file_data)
+        download_path.parent.mkdir(parents=True, exist_ok=True)
+        logger.info(f"Writing {file_data.identifier} to {download_path}")
+        try:
+            with self.workspace.dbfs.download(path=file_data.identifier) as c:
+                read_content = c._read_handle.read()
+            with open(download_path, "wb") as f:
+                f.write(read_content)
+        except Exception as e:
+            logger.error(f"failed to download file {file_data.identifier}: {e}", exc_info=True)
+            raise SourceConnectionNetworkError(f"failed to download file {file_data.identifier}")
+        if (
+                file_data.metadata.date_modified
+                and self.is_float(file_data.metadata.date_modified)
+                and file_data.metadata.date_created
+                and self.is_float(file_data.metadata.date_created)
+        ):
+            date_modified = float(file_data.metadata.date_modified)
+            date_created = float(file_data.metadata.date_created)
+            os.utime(download_path, times=(date_created, date_modified))
+        return download_path
 
 
 @dataclass
@@ -155,3 +269,12 @@ databricks_volumes_destination_entry = DestinationRegistryEntry(
     uploader=DatabricksVolumesUploader,
     uploader_config=DatabricksVolumesUploaderConfig,
 )
+
+databricks_volumes_source_entry = SourceRegistryEntry(
+    connection_config=DatabricksVolumesConnectionConfig,
+    indexer=DatabricksVolumesIndexer,
+    indexer_config=DatabricksVolumesIndexerConfig,
+    downloader=DatabricksVolumesDownloader,
+    downloader_config=DatabricksVolumesDownloaderConfig,
+)
+
