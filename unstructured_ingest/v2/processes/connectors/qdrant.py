7c29ac17afe108e5931aeb9234c5f749592b3813
Guilherme <guilherme.landim@unstructured.io>
2024-10-17 17:17:04 -0300

    taking optional out from access config

--- unstructured_ingest/v2/processes/connectors/qdrant.py
+++ unstructured_ingest/v2/processes/connectors/qdrant.py
@@ -42,7 +42,7 @@ class QdrantConnectionConfig(ConnectionConfig):
     port: Optional[int] = Field(default=6333, description="Port")
     grpc_port: Optional[int] = Field(default=6334, description="GRPC Port")
     prefer_grpc: Optional[bool] = Field(default=False, description="Prefer GRCP")
-    https: Optional[bool] = Field(default=None, description="HTTPS")
+    https: Optional[bool] = Field(default=None, description="HTTPS")    
     prefix: Optional[str] = Field(default=None, description="Prefix")
     timeout: Optional[float] = Field(default=None, description="TimeOut time")
     host: Optional[str] = Field(default=None, description="Host")
@@ -50,7 +50,7 @@ class QdrantConnectionConfig(ConnectionConfig):
     force_disable_check_same_thread: Optional[bool] = Field(
         default=False, description="Force disable check for same thread"
     )
-    access_config: Optional[QdrantAccessConfig] = Field(default=None, description="Access Config")
+    access_config: QdrantAccessConfig = Field(default=None, description="Access Config")
 
 
 class QdrantUploadStagerConfig(UploadStagerConfig):
