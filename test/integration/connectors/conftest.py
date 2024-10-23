791196ffa07679f04d0ee3e744b246978b12818e
Maciej Kurzawa <68014263+mackurzawa@users.noreply.github.com>
2024-10-23 16:53:32 +0200

    feat/add Delta Tables Destination Connector v2 (#163)
    * feat add unfinished first version
    
    * fix add pass
    
    * update requirements
    
    * delta table connector for s3
    
    * fix add AWS REGION
    
    * fix add new test to test-dest.sh
    
    * code more readable
    
    * remove unused import
    
    * address feedback
    
    * lint
    
    * change file extension in stager
    
    * add comment for locks in S3
    
    * remove mode, engine, schema_mode from configs
    
    * allow for dynamic stagers
    
    * add databricks volumes src to __init__ - automatic merge didnt add it
    
    * delete pandas from delta-table requirements
    
    * add integration test for delta-table-s3
    
    * consider aws_region as non-sensitive parameter
    
    * provide better description for table_uri
    
    * change import location, remove requires_dependencies before precheck for uploader
    
    * use fsspec to clean up after integration test
    
    * bump version
    
    * bump changelog version, implement uploader precheck, change bash test s3 destination folder
    
    * change fsspec import location
    
    * create update_storage_options method
    
    * upload Delta tables to folders with adequate names
    
    * linter
    
    * modify local e2e test to leverage new naming convention
    
    * Add type annotations to update_storage_options method
    
    Co-authored-by: Roman Isecke <136338424+rbiseck3@users.noreply.github.com>
    
    * change uploader precheck to write empty file to s3
    
    * reformat blank lines
    
    * Delta Table local test leveraging new testing framework
    
    * deleted test_e2e/dest/delta-table-s3.sh, test_e2e/dest/delta-table.sh, test_e2e/python/test-ingest-delta-table-output.py, and updated test_e2e/test-dest.sh
    
    ---------
    
    Co-authored-by: Hubert Rutkowski <157481729+hubert-rutkowski85@users.noreply.github.com>
    Co-authored-by: Roman Isecke <136338424+rbiseck3@users.noreply.github.com>

--- test/integration/connectors/conftest.py
+++ test/integration/connectors/conftest.py
@@ -1,6 +1,10 @@
+import tempfile
 from pathlib import Path
+from typing import Generator
 
 import pytest
+
+from unstructured_ingest.v2.logger import logger
 
 FILENAME = "DA-1p-with-duplicate-pages.pdf.json"
 
@@ -13,3 +17,12 @@ def upload_file() -> Path:
     assert upload_file.exists()
     assert upload_file.is_file()
     return upload_file
+
+
+@pytest.fixture
+def temp_dir() -> Generator[Path, None, None]:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_path = Path(temp_dir)
+        logger.info(f"Created temp dir '{temp_path}'")
+        yield temp_path
+        logger.info(f"Removing temp dir '{temp_path}'")
