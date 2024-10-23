e26e2f02f33ff4cc83945ecd205b5f7352a873cb
Roman Isecke <136338424+rbiseck3@users.noreply.github.com>
2024-10-23 12:05:47 -0400

    feat/snowflake connector (#191)
    * Add source connector with test using localstack
    
    * Add destination connector with test using localstack
    
    * bump changelog
    
    * remove hard coded token
    
    * Add support to check the contents of downloaded files in integration tests
    
    * use dataframe based equality for csv files
    
    * Add additional printing for debugging
    
    * populate sql db with deterministic content
    
    * update sql tests

--- unstructured_ingest/v2/processes/connectors/sql/postgres.py
+++ unstructured_ingest/v2/processes/connectors/sql/postgres.py
@@ -1,15 +1,16 @@
+from contextlib import contextmanager
 from dataclasses import dataclass, field
-from pathlib import Path
-from typing import TYPE_CHECKING, Any, Optional
+from typing import TYPE_CHECKING, Any, Generator, Optional
 
-import numpy as np
-import pandas as pd
 from pydantic import Field, Secret
 
 from unstructured_ingest.utils.dep_check import requires_dependencies
 from unstructured_ingest.v2.interfaces import FileData
 from unstructured_ingest.v2.logger import logger
-from unstructured_ingest.v2.processes.connector_registry import DestinationRegistryEntry
+from unstructured_ingest.v2.processes.connector_registry import (
+    DestinationRegistryEntry,
+    SourceRegistryEntry,
+)
 from unstructured_ingest.v2.processes.connectors.sql.sql import (
     _DATE_COLUMNS,
     SQLAccessConfig,
@@ -27,6 +28,7 @@ from unstructured_ingest.v2.processes.connectors.sql.sql import (
 
 if TYPE_CHECKING:
     from psycopg2.extensions import connection as PostgresConnection
+    from psycopg2.extensions import cursor as PostgresCursor
 
 CONNECTOR_TYPE = "postgres"
 
@@ -48,18 +50,33 @@ class PostgresConnectionConfig(SQLConnectionConfig):
     port: Optional[int] = Field(default=5432, description="DB host connection port")
     connector_type: str = Field(default=CONNECTOR_TYPE, init=False)
 
+    @contextmanager
     @requires_dependencies(["psycopg2"], extras="postgres")
-    def get_connection(self) -> "PostgresConnection":
+    def get_connection(self) -> Generator["PostgresConnection", None, None]:
         from psycopg2 import connect
 
         access_config = self.access_config.get_secret_value()
-        return connect(
+        connection = connect(
             user=self.username,
             password=access_config.password,
             dbname=self.database,
             host=self.host,
             port=self.port,
         )
+        try:
+            yield connection
+        finally:
+            connection.commit()
+            connection.close()
+
+    @contextmanager
+    def get_cursor(self) -> Generator["PostgresCursor", None, None]:
+        with self.get_connection() as connection:
+            cursor = connection.cursor()
+            try:
+                yield cursor
+            finally:
+                cursor.close()
 
 
 class PostgresIndexerConfig(SQLIndexerConfig):
@@ -71,16 +88,6 @@ class PostgresIndexer(SQLIndexer):
     connection_config: PostgresConnectionConfig
     index_config: PostgresIndexerConfig
     connector_type: str = CONNECTOR_TYPE
-
-    def _get_doc_ids(self) -> list[str]:
-        connection = self.connection_config.get_connection()
-        with connection.cursor() as cursor:
-            cursor.execute(
-                f"SELECT {self.index_config.id_column} FROM {self.index_config.table_name}"
-            )
-            results = cursor.fetchall()
-            ids = [result[0] for result in results]
-            return ids
 
 
 class PostgresDownloaderConfig(SQLDownloaderConfig):
@@ -97,8 +104,7 @@ class PostgresDownloader(SQLDownloader):
         table_name = file_data.additional_metadata["table_name"]
         id_column = file_data.additional_metadata["id_column"]
         ids = file_data.additional_metadata["ids"]
-        connection = self.connection_config.get_connection()
-        with connection.cursor() as cursor:
+        with self.connection_config.get_cursor() as cursor:
             fields = ",".join(self.download_config.fields) if self.download_config.fields else "*"
             query = "SELECT {fields} FROM {table_name} WHERE {id_column} in ({ids})".format(
                 fields=fields,
@@ -130,6 +136,7 @@ class PostgresUploader(SQLUploader):
     upload_config: PostgresUploaderConfig = field(default_factory=PostgresUploaderConfig)
     connection_config: PostgresConnectionConfig
     connector_type: str = CONNECTOR_TYPE
+    values_delimiter: str = "%s"
 
     def prepare_data(
         self, columns: list[str], data: tuple[tuple[Any, ...], ...]
@@ -148,25 +155,14 @@ class PostgresUploader(SQLUploader):
             output.append(tuple(parsed))
         return output
 
-    def upload_contents(self, path: Path) -> None:
-        df = pd.read_json(path, orient="records", lines=True)
-        logger.debug(f"uploading {len(df)} entries to {self.connection_config.database} ")
-        df.replace({np.nan: None}, inplace=True)
 
-        columns = tuple(df.columns)
-        stmt = f"INSERT INTO {self.upload_config.table_name} ({','.join(columns)}) \
-                VALUES({','.join(['%s' for x in columns])})"  # noqa E501
-
-        for rows in pd.read_json(
-            path, orient="records", lines=True, chunksize=self.upload_config.batch_size
-        ):
-            with self.connection_config.get_connection() as conn:
-                values = self.prepare_data(columns, tuple(rows.itertuples(index=False, name=None)))
-                with conn.cursor() as cur:
-                    cur.executemany(stmt, values)
-
-                conn.commit()
-
+postgres_source_entry = SourceRegistryEntry(
+    connection_config=PostgresConnectionConfig,
+    indexer_config=PostgresIndexerConfig,
+    indexer=PostgresIndexer,
+    downloader_config=PostgresDownloaderConfig,
+    downloader=PostgresDownloader,
+)
 
 postgres_destination_entry = DestinationRegistryEntry(
     connection_config=PostgresConnectionConfig,
