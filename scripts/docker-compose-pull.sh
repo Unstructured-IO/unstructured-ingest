e3c2215130fcc2e8c97cb07a953d8dde24557545
Roman Isecke <roman@unstructured.io>
2024-10-24 16:47:26 -0400

    shell tidy

--- scripts/docker-compose-pull.sh
+++ scripts/docker-compose-pull.sh
@@ -9,12 +9,12 @@ fi
 path=$1
 type=$2
 
-if ! { [ $type == "source" ] || [ $type == "destination" ]; }; then
+if ! { [ "$type" == "source" ] || [ "$type" == "destination" ]; }; then
   echo "usage: $0 path [source|destination]"
   exit 1
 fi
 
-for docker_file in $(find $path -type f -name "docker-compose.yaml" | grep $type); do
+for docker_file in $(find "$path" -type f -name "docker-compose.yaml" | grep "$type"); do
   echo "pulling images from $docker_file"
-  docker compose -f $docker_file pull
+  docker compose -f "$docker_file" pull
 done
