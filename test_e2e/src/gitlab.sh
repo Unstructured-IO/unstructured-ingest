dd4c7241cbc330b8d6ce064b262a26acdddc9f60
Filip Knefel <filip@unstructured.io>
2024-10-30 13:13:39 +0100

    Fix number of expected files in test

--- test_e2e/src/gitlab.sh
+++ test_e2e/src/gitlab.sh
@@ -45,5 +45,5 @@ PYTHONPATH=${PYTHONPATH:-.} "$RUN_SCRIPT" \
   --recursive \
   --work-dir "$WORK_DIR"
 
-"$SCRIPT_DIR"/check-num-files-output.sh 2 $OUTPUT_FOLDER_NAME
+"$SCRIPT_DIR"/check-num-files-output.sh 1 $OUTPUT_FOLDER_NAME
 "$SCRIPT_DIR"/check-diff-expected-output.py --output-folder-name $OUTPUT_FOLDER_NAME
