6da5cab296d3caf3c32de5393f9eca506befe6a4
Roman Isecke <roman@unstructured.io>
2024-11-08 14:13:14 -0500

    replace script with python using multiprocessing to speed things up

--- test_e2e/test-help.py
+++ test_e2e/test-help.py
@@ -1,4 +1,30 @@
+import multiprocessing as mp
+import subprocess
+from pathlib import Path
+
 from unstructured_ingest.cli.cli import get_cmd
+
+
+def get_main_file() -> Path:
+    script_path = Path(__file__).parents[1]
+    main_file = script_path / "unstructured_ingest" / "main.py"
+    assert main_file.exists()
+    assert main_file.is_file()
+    return main_file
+
+
+def run_command(cmd):
+    resp = subprocess.run(
+        cmd,
+        shell=True,
+        capture_output=True,
+    )
+    if resp.returncode != 0:
+        print(f"failed to run: {cmd}")
+        print("STDOUT: {}".format(resp.stdout.decode("utf-8")))
+        print("STDERR: {}".format(resp.stderr.decode("utf-8")))
+        raise subprocess.CalledProcessError(resp.returncode, resp.args, resp.stdout, resp.stderr)
+
 
 if __name__ == "__main__":
     cmd = get_cmd()
@@ -11,6 +37,13 @@ if __name__ == "__main__":
     dest_command_dict = first_dest_command.commands
     dest_command_labels = list(dest_command_dict.keys())
 
-    print(src_command_labels)
+    cmds = []
+    main_file = get_main_file()
+    for src_command_label in src_command_labels:
+        cmds.append(f"{main_file} {src_command_label} --help")
 
-    print(dest_command_labels)
+    for dest_command_label in dest_command_labels:
+        cmds.append(f"{main_file} {first_src_command_label} {dest_command_label} --help")
+
+    with mp.Pool(processes=mp.cpu_count()) as pool:
+        pool.map(run_command, cmds)
