c20fede26d7c3b64116df0b3e57428b38e037496
Roman Isecke <roman@unstructured.io>
2024-11-08 18:07:07 -0500

    create adjacency json in stager

--- setup.py
+++ setup.py
@@ -106,6 +106,7 @@ connectors_reqs = {
     "kdbai": load_requirements("requirements/connectors/kdbai.in"),
     "milvus": load_requirements("requirements/connectors/milvus.in"),
     "mongodb": load_requirements("requirements/connectors/mongodb.in"),
+    "neo4j": load_requirements("requirements/connectors/neo4j.in"),
     "notion": load_requirements("requirements/connectors/notion.in"),
     "onedrive": load_requirements("requirements/connectors/onedrive.in"),
     "opensearch": load_requirements("requirements/connectors/opensearch.in"),
--- unstructured_ingest/v2/processes/connectors/neo4j.py
+++ unstructured_ingest/v2/processes/connectors/neo4j.py
@@ -1,12 +1,17 @@
+import asyncio
 import json
-from dataclasses import dataclass, field
+import uuid
+from contextlib import asynccontextmanager
+from dataclasses import dataclass
 from pathlib import Path
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Any, AsyncGenerator, Union
 
+import networkx as nx
 from cymple import QueryBuilder
-from pydantic import Field, Secret
+from pydantic import BaseModel, Field, Secret
 
 from unstructured_ingest.error import DestinationConnectionError
+from unstructured_ingest.utils.chunking import elements_from_base64_gzipped_json
 from unstructured_ingest.utils.dep_check import requires_dependencies
 from unstructured_ingest.v2.interfaces import (
     AccessConfig,
@@ -23,71 +28,127 @@ from unstructured_ingest.v2.processes.connector_registry import (
 )
 
 if TYPE_CHECKING:
-    from neo4j import AsyncDriver, Driver
+    from neo4j import AsyncDriver, Auth
 
 
 CONNECTOR_TYPE = "neo4j"
 
 
 class Neo4jAccessConfig(AccessConfig):
-    uri: str
-    user: str
     password: str
 
 
 class Neo4jConnectionConfig(ConnectionConfig):
     access_config: Secret[Neo4jAccessConfig]
     connector_type: str = Field(default=CONNECTOR_TYPE, init=False)
-    database: Optional[str] = Field(default=None)
+    username: str
+    uri: str = Field(description="the connection URI for the driver")
 
     @requires_dependencies(["neo4j"], extras="neo4j")
-    @DestinationConnectionError.wrap
-    def get_driver(self) -> "Driver":
-        from neo4j import GraphDatabase
+    def get_auth(self) -> "Auth":
+        from neo4j import Auth
 
-        access_config = self.access_config.get_secret_value()
-        return GraphDatabase.driver(
-            uri=access_config.uri,
-            auth=(access_config.user, access_config.password),
-            database=self.database,
-        )
+        return Auth("basic", self.username, self.access_config.get_secret_value().password)
+
+    def get_drive_configs(self) -> dict:
+        return {
+            "uri": self.uri,
+            "auth": self.get_auth(),
+        }
 
     @requires_dependencies(["neo4j"], extras="neo4j")
-    @DestinationConnectionError.wrap
-    def get_async_driver(self) -> "AsyncDriver":
+    @asynccontextmanager
+    async def get_client(self) -> AsyncGenerator["AsyncDriver", None]:
         from neo4j import AsyncGraphDatabase
 
-        access_config = self.access_config.get_secret_value()
-        return AsyncGraphDatabase.driver(
-            uri=access_config.uri,
-            auth=(access_config.user, access_config.password),
-            database=self.database,
-        )
+        driver = AsyncGraphDatabase.driver(**self.get_drive_configs())
+        try:
+            yield driver
+        finally:
+            await driver.close()
 
 
 class Neo4jUploadStagerConfig(UploadStagerConfig):
     pass
 
 
+class Node(BaseModel):
+    ref_name: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    labels: Union[list[str], str] = Field(default_factory=list)
+    properties: dict = Field(default_factory=dict)
+
+    def __hash__(self):
+        return hash(self.ref_name)
+
+
 @dataclass
 class Neo4jUploadStager(UploadStager):
-    upload_stager_config: Neo4jUploadStagerConfig = field(
-        default_factory=lambda: Neo4jUploadStagerConfig()
-    )
+    upload_stager_config: Neo4jUploadStagerConfig = Field(default_factory=Neo4jUploadStagerConfig)
 
-    def is_async(self):
-        return True
+    def has_children(self, element: dict) -> bool:
+        return "orig_elements" in element.get("metadata", {})
 
-    def run_async(
-        self, elements_filepath, file_data: FileData, output_dir, output_filename, **kwargs
-    ):
-        return super().run_async(
-            elements_filepath, file_data, output_dir, output_filename, **kwargs
+    def get_children(self, element: dict) -> list[dict]:
+        orig_elements = element.get("metadata", {}).get("orig_elements")
+        return elements_from_base64_gzipped_json(raw_s=orig_elements)
+
+    def element_to_node(self, element: dict) -> Node:
+        properties = {"id": element["element_id"], "text": element["text"]}
+
+        if embeddings := element.get("embeddings"):
+            properties["embeddings"] = embeddings
+
+        return Node(
+            properties=properties,
         )
+
+    def create_graph(self, elements: list[dict], parent_node: Node) -> nx.Graph:
+        graph = nx.Graph()
+        graph.add_node(parent_node)
+        for element in elements:
+            element_type = element["type"]
+            element_type_node = Node(ref_name=element_type)
+            if element_type_node not in graph.nodes:
+                graph.add_node(element_type_node)
+            element_node = self.element_to_node(element=element)
+            graph.add_edge(element_node, element_type_node)
+            if self.has_children(element):
+                element_node.labels.append("chunked_element")
+                graph.add_node(element_node)
+                for child in self.get_children(element):
+                    child_element_node = self.element_to_node(element=child)
+                    graph.add_node(child_element_node)
+                    graph.add_edge(element_node, child_element_node)
+            graph.add_edge(element_node, parent_node)
+        return graph
+
+    def run(
+        self,
+        elements_filepath: Path,
+        file_data: FileData,
+        output_dir: Path,
+        output_filename: str,
+        **kwargs: Any,
+    ) -> Path:
+        with elements_filepath.open() as file:
+            elements = json.load(file)
+        file_node = Node(ref_name="file")
+        graph = self.create_graph(elements, file_node)
+        nodes = graph.nodes()
+        file_node.model_dump()
+        nodes_dict = [n.model_dump() for n in nodes]
+        edges = list(graph.edges())
+        edges_dict = [[edge[0].ref_name, edge[1].ref_name] for edge in edges]
+        data = {"nodes": nodes_dict, "edges": edges_dict}
+        output_path = Path(output_dir) / Path(f"{output_filename}.json")
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(output_path, "w") as output_file:
+            json.dump(data, output_file, indent=2)
+        return output_path
 
 
 class Neo4jUploaderConfig(UploaderConfig):
-    pass
+    database: str = Field(description="database to write to")
 
 
 @dataclass
@@ -96,34 +157,41 @@ class Neo4jUploader(Uploader):
     connection_config: Neo4jConnectionConfig
     connector_type: str = CONNECTOR_TYPE
 
-    @DestinationConnectionError.wrap
-    def precheck(self) -> None:
-        driver = self.connection_config.get_driver()
-        if not driver.verify_authentication():
-            raise DestinationConnectionError("Invalid authentication information.")
-
-    def run(self, path, file_data, **kwargs):
-        raise NotImplementedError
-
-    async def run_async(self, path: Path, file_data: FileData, **kwargs) -> None:  # type: ignore[override]
-        driver = self.connection_config.get_async_driver()
-        logger.info("Creating new File node")
-        await self._create_file_node(driver, file_data)
-
-        with open(path) as file:
-            element_dicts = json.load(file)
-            for element_dict in element_dicts:
-                logger.info("Creating new Element node")
-                await self._create_element_node(driver, element_dict, file_data.identifier)
-
-        await driver.close()
-
     def is_async(self):
         return True
 
+    @DestinationConnectionError.wrap
+    def precheck(self) -> None:
+        async def verify_auth():
+            async with self.connection_config.get_client() as client:
+                if not client.verify_authentication():
+                    raise DestinationConnectionError("Invalid authentication information.")
+
+        asyncio.run(verify_auth())
+
+    def get_elements(self, path: Path) -> dict:
+        with path.open() as file:
+            return json.load(file)
+
+    async def run_async(self, path: Path, file_data: FileData, **kwargs) -> None:
+        async with self.connection_config.get_client() as client:
+            logger.info("Creating new file node")
+            await self._create_file_node(client, file_data)
+
+            elements = self.get_elements(path=path)
+            file_id = file_data.identifier
+            await asyncio.gather(
+                *[
+                    self._create_element_node(
+                        client=client, element_dict=element_dict, file_id=file_id
+                    )
+                    for element_dict in elements
+                ]
+            )
+
     async def _create_file_node(
         self,
-        driver: "AsyncDriver",
+        client: "AsyncDriver",
         file_data: FileData,
     ) -> None:
         properties = {"id": file_data.identifier}
@@ -138,12 +206,18 @@ class Neo4jUploader(Uploader):
         builder = QueryBuilder()
         query = builder.create().node(labels="File", properties=properties)
 
-        print(query)
+        await client.execute_query(query.get())
 
-        await driver.execute_query(query.get())
+    def get_element_properties(self, element_dict) -> dict:
+        properties = {"id": element_dict["element_id"], "text": element_dict["text"]}
+
+        if embeddings := element_dict.get("embeddings"):
+            properties["embeddings"] = embeddings
+
+        return properties
 
     async def _create_element_node(
-        self, driver: "AsyncDriver", element_dict: dict, file_id: str
+        self, client: "AsyncDriver", element_dict: dict, file_id: str
     ) -> None:
         properties = {"id": element_dict["element_id"], "text": element_dict["text"]}
 
@@ -161,13 +235,11 @@ class Neo4jUploader(Uploader):
             .node(ref_name="f")
         )
 
-        await driver.execute_query(query)
+        await client.execute_query(query)
 
 
 neo4j_destination_entry = DestinationRegistryEntry(
     connection_config=Neo4jConnectionConfig,
     uploader=Neo4jUploader,
     uploader_config=Neo4jUploaderConfig,
-    upload_stager=Neo4jUploadStager,
-    upload_stager_config=Neo4jUploadStagerConfig,
 )
