2f4f244890985e1e164043bee445ef6d6debdb36
Adithya Krishnan <krishsandeep@gmail.com>
2024-11-18 10:33:52 +0100

    reuse duckdb base classes

--- unstructured_ingest/v2/processes/connectors/motherduck.py
+++ unstructured_ingest/v2/processes/connectors/motherduck.py
@@ -1,5 +1,3 @@
-import json
-import uuid
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, Callable, Literal, Optional
@@ -14,12 +12,14 @@ from unstructured_ingest.v2.interfaces import (
     ConnectionConfig,
     FileData,
     Uploader,
-    UploaderConfig,
-    UploadStager,
-    UploadStagerConfig,
 )
 from unstructured_ingest.v2.logger import logger
 from unstructured_ingest.v2.processes.connector_registry import DestinationRegistryEntry
+from unstructured_ingest.v2.processes.connectors.duckdb import (
+    DuckDBUploadStagerConfig,
+    DuckDBUploadStager,
+    DuckDBUploaderConfig,
+)
 
 if TYPE_CHECKING:
     from duckdb import DuckDBPyConnection as MotherDuckConnection
@@ -31,7 +31,7 @@ class MotherDuckAccessConfig(AccessConfig):
     md_token: Optional[str] = Field(default=None, description="MotherDuck token")
 
 
-class MotherDuckConnectionConfig(ConnectionConfig):    
+class MotherDuckConnectionConfig(ConnectionConfig):
     connector_type: str = Field(default=CONNECTOR_TYPE, init=False)
     database: Optional[str] = Field(
         default=None,
@@ -49,7 +49,7 @@ class MotherDuckConnectionConfig(ConnectionConfig):
         default=MotherDuckAccessConfig(), validate_default=True
     )
 
-    def __post_init__(self):        
+    def __post_init__(self):
         if self.database is None:
             raise ValueError(
                 "A MotherDuck connection requires a database (string) to be passed "
@@ -62,105 +62,18 @@ class MotherDuckConnectionConfig(ConnectionConfig):
             )
 
 
-class MotherDuckUploadStagerConfig(UploadStagerConfig):
+class MotherDuckUploadStagerConfig(DuckDBUploadStagerConfig):
     pass
 
 
-_COLUMNS = (
-    "id",
-    "element_id",
-    "text",
-    "embeddings",
-    "type",
-    "system",
-    "layout_width",
-    "layout_height",
-    "points",
-    "url",
-    "version",
-    "date_created",
-    "date_modified",
-    "date_processed",
-    "permissions_data",
-    "record_locator",
-    "category_depth",
-    "parent_id",
-    "attached_filename",
-    "filetype",
-    "last_modified",
-    "file_directory",
-    "filename",
-    "languages",
-    "page_number",
-    "links",
-    "page_name",
-    "link_urls",
-    "link_texts",
-    "sent_from",
-    "sent_to",
-    "subject",
-    "section",
-    "header_footer_type",
-    "emphasized_text_contents",
-    "emphasized_text_tags",
-    "text_as_html",
-    "regex_metadata",
-    "detection_class_prob",
-)
-
-# _DATE_COLUMNS = ("date_created", "date_modified", "date_processed", "last_modified")
-
-
 @dataclass
-class MotherDuckUploadStager(UploadStager):
+class MotherDuckUploadStager(DuckDBUploadStager):
     upload_stager_config: MotherDuckUploadStagerConfig = field(
         default_factory=lambda: MotherDuckUploadStagerConfig()
     )
 
-    def run(
-        self,
-        elements_filepath: Path,
-        file_data: FileData,
-        output_dir: Path,
-        output_filename: str,
-        **kwargs: Any,
-    ) -> Path:
-        with open(elements_filepath) as elements_file:
-            elements_contents: list[dict] = json.load(elements_file)
-        output_path = Path(output_dir) / Path(f"{output_filename}.json")
-        output_path.parent.mkdir(parents=True, exist_ok=True)
 
-        output = []
-        for data in elements_contents:
-            metadata: dict[str, Any] = data.pop("metadata", {})
-            data_source = metadata.pop("data_source", {})
-            coordinates = metadata.pop("coordinates", {})
-
-            data.update(metadata)
-            data.update(data_source)
-            data.update(coordinates)
-
-            data["id"] = str(uuid.uuid4())
-
-            # remove extraneous, not supported columns
-            data = {k: v for k, v in data.items() if k in _COLUMNS}
-
-            output.append(data)
-
-        df = pd.DataFrame.from_dict(output)
-
-        for column in filter(
-            lambda x: x in df.columns,
-            ("version", "page_number", "regex_metadata"),
-        ):
-            df[column] = df[column].apply(str)
-
-        with output_path.open("w") as output_file:
-            df.to_json(output_file, orient="records", lines=True)
-        return output_path
-
-
-class MotherDuckUploaderConfig(UploaderConfig):
+class MotherDuckUploaderConfig(DuckDBUploaderConfig):
     batch_size: int = Field(default=50, description="[Not-used] Number of records per batch")
 
 
