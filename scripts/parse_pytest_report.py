5b5d27f4d1240fe6fb4c6a49292bf4923c93a295
Roman Isecke <roman@unstructured.io>
2024-11-21 15:06:29 -0500

    fix errors

--- scripts/parse_pytest_report.py
+++ scripts/parse_pytest_report.py
@@ -9,12 +9,14 @@ if __name__ == "__main__":
     print(f"parsing report contents from: {report_path}")
     with report_path.open() as f:
         report_data = json.load(f)
-    skipped = report_data.get("summary", {}).get("skipped, 0")
+    skipped = report_data.get("summary", {}).get("skipped", 0)
     if skipped == 0:
         print("No skipped tests")
         exit()
     tests = report_data.get("tests", [])
     skipped_tests = [test for test in tests if test["outcome"] == "skipped"]
+    if not skipped_tests:
+        print("No skipped tests")
     print(f"Updating github step summary with {len(skipped_tests)} skipped tests")
     with open(os.environ["GITHUB_STEP_SUMMARY"], "a") as fh:
         print("### Pytest Skipped Test Warning", file=fh)
