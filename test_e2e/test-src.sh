aee72c54452e61da87d44bbdfee2b4ad475d76ce
Mateusz Kuprowski <mateusz.kuprowski-ext@hexagon.com>
2024-11-22 10:11:39 +0100

    Fixed test-src perms

--- test_e2e/test-src.sh
+++ test_e2e/test-src.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 SKIPPED_FILES_LOG=$SCRIPT_DIR/skipped-files.txt
 # If the file already exists, reset it
 if [ -f "$SKIPPED_FILES_LOG" ]; then
-	rm "$SKIPPED_FILES_LOG"
+  rm "$SKIPPED_FILES_LOG"
 fi
 touch "$SKIPPED_FILES_LOG"
 cd "$SCRIPT_DIR"/.. || exit 1
@@ -50,24 +50,25 @@ all_tests=(
   'sharepoint-with-permissions.sh'
   'hubspot.sh'
   'sftp.sh'
+  'mongodb.sh'
 )
 
 full_python_matrix_tests=(
-	#  'sharepoint.sh'
-	's3.sh'
-	'google-drive.sh'
-	'gcs.sh'
-	'azure.sh'
+  #  'sharepoint.sh'
+  's3.sh'
+  'google-drive.sh'
+  'gcs.sh'
+  'azure.sh'
 )
 
 CURRENT_TEST="none"
 
 function print_last_run() {
-	if [ "$CURRENT_TEST" != "none" ]; then
-		echo "Last ran script: $CURRENT_TEST"
-	fi
-	echo "######## SKIPPED TESTS: ########"
-	cat "$SKIPPED_FILES_LOG"
+  if [ "$CURRENT_TEST" != "none" ]; then
+    echo "Last ran script: $CURRENT_TEST"
+  fi
+  echo "######## SKIPPED TESTS: ########"
+  cat "$SKIPPED_FILES_LOG"
 }
 
 trap print_last_run EXIT
@@ -75,43 +76,43 @@ trap print_last_run EXIT
 python_version=$(python --version 2>&1)
 
 tests_to_ignore=(
-	'notion.sh'
+  'notion.sh'
 )
 
 if [ -z "$UNS_PAID_API_KEY" ]; then
-	echo "Skipping source ingest tests because UNS_PAID_API_KEY env var is not set"
-	exit
+  echo "Skipping source ingest tests because UNS_PAID_API_KEY env var is not set"
+  exit
 fi
 
 for test in "${all_tests[@]}"; do
-	CURRENT_TEST="$test"
-	# IF: python_version is not 3.10 (wildcarded to match any subminor version) AND the current test is not in full_python_matrix_tests
-	# Note: to test we expand the full_python_matrix_tests array to a string and then regex match the current test
-	if [[ "$python_version" != "Python 3.10"* ]] && [[ ! "${full_python_matrix_tests[*]}" =~ $test ]]; then
-		echo "--------- SKIPPING SCRIPT $test ---------"
-		continue
-	fi
-	echo "--------- RUNNING SCRIPT $test ---------"
-	echo "Running ./test_e2e/$test"
-	./test_e2e/src/"$test"
-	rc=$?
-	if [[ $rc -eq 8 ]]; then
-		echo "$test (skipped due to missing env var)" | tee -a "$SKIPPED_FILES_LOG"
-	else
-		# Check if the test is in tests_to_ignore
-		ignore_test=false
-		for ignore in "${tests_to_ignore[@]}"; do
-			if [[ "$ignore" == "$test" ]]; then
-				ignore_test=true
-				break
-			fi
-		done
-		if $ignore_test; then
-			echo "$test (skipped checking error code: $rc)" | tee -a "$SKIPPED_FILES_LOG"
-			continue
-		elif [[ $rc -ne 0 ]]; then
-			exit $rc
-		fi
-	fi
-	echo "--------- FINISHED SCRIPT $test ---------"
+  CURRENT_TEST="$test"
+  # IF: python_version is not 3.10 (wildcarded to match any subminor version) AND the current test is not in full_python_matrix_tests
+  # Note: to test we expand the full_python_matrix_tests array to a string and then regex match the current test
+  if [[ "$python_version" != "Python 3.10"* ]] && [[ ! "${full_python_matrix_tests[*]}" =~ $test ]]; then
+    echo "--------- SKIPPING SCRIPT $test ---------"
+    continue
+  fi
+  echo "--------- RUNNING SCRIPT $test ---------"
+  echo "Running ./test_e2e/$test"
+  ./test_e2e/src/"$test"
+  rc=$?
+  if [[ $rc -eq 8 ]]; then
+    echo "$test (skipped due to missing env var)" | tee -a "$SKIPPED_FILES_LOG"
+  else
+    # Check if the test is in tests_to_ignore
+    ignore_test=false
+    for ignore in "${tests_to_ignore[@]}"; do
+      if [[ "$ignore" == "$test" ]]; then
+        ignore_test=true
+        break
+      fi
+    done
+    if $ignore_test; then
+      echo "$test (skipped checking error code: $rc)" | tee -a "$SKIPPED_FILES_LOG"
+      continue
+    elif [[ $rc -ne 0 ]]; then
+      exit $rc
+    fi
+  fi
+  echo "--------- FINISHED SCRIPT $test ---------"
 done
