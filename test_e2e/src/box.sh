df6dc3c2abf9e9f647df767adee453217854ae0f
Michal Martyniak <michal.martyniak@deepsense.ai>
2024-11-22 10:51:16 +0100

    re-do shfmt

--- test_e2e/src/box.sh
+++ test_e2e/src/box.sh
@@ -41,20 +41,20 @@ fi
 
 RUN_SCRIPT=${RUN_SCRIPT:-./unstructured_ingest/main.py}
 PYTHONPATH=${PYTHONPATH:-.} "$RUN_SCRIPT" \
-box \
---api-key "$UNS_PAID_API_KEY" \
---partition-by-api \
---partition-endpoint "https://api.unstructuredapp.io" \
---download-dir "$DOWNLOAD_DIR" \
---box-app-config "$BOX_APP_CONFIG" \
---remote-url box://utic-test-ingest-fixtures \
---metadata-exclude coordinates,filename,file_directory,metadata.data_source.date_processed,metadata.last_modified,metadata.detection_class_prob,metadata.parent_id,metadata.category_depth \
---output-dir "$OUTPUT_DIR" \
---num-processes "$max_processes" \
---preserve-downloads \
---recursive \
---reprocess \
---verbose \
---work-dir "$WORK_DIR"
+	box \
+	--api-key "$UNS_PAID_API_KEY" \
+	--partition-by-api \
+	--partition-endpoint "https://api.unstructuredapp.io" \
+	--download-dir "$DOWNLOAD_DIR" \
+	--box-app-config "$BOX_APP_CONFIG" \
+	--remote-url box://utic-test-ingest-fixtures \
+	--metadata-exclude coordinates,filename,file_directory,metadata.data_source.date_processed,metadata.last_modified,metadata.detection_class_prob,metadata.parent_id,metadata.category_depth \
+	--output-dir "$OUTPUT_DIR" \
+	--num-processes "$max_processes" \
+	--preserve-downloads \
+	--recursive \
+	--reprocess \
+	--verbose \
+	--work-dir "$WORK_DIR"
 
 "$SCRIPT_DIR"/check-diff-expected-output.py --output-folder-name $OUTPUT_FOLDER_NAME
