73b53ad0a2cfba54f1898f48740c473f9bdde5c9
Roman Isecke <roman@unstructured.io>
2024-11-22 09:19:51 -0500

    add missing extra

--- setup.py
+++ setup.py
@@ -104,6 +104,7 @@ connectors_reqs = {
     "jira": load_requirements("requirements/connectors/jira.in"),
     "kafka": load_requirements("requirements/connectors/kafka.in"),
     "kdbai": load_requirements("requirements/connectors/kdbai.in"),
+    "lancedb": load_requirements("requirements/connectors/lancedb.in"),
     "milvus": load_requirements("requirements/connectors/milvus.in"),
     "mongodb": load_requirements("requirements/connectors/mongodb.in"),
     "notion": load_requirements("requirements/connectors/notion.in"),
