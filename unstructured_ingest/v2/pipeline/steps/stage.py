52a5414554c4413311b65c435395967106f62c94
Roman Isecke <roman@unstructured.io>
2024-12-16 11:33:38 -0500

    fix stage pipeline step to maintain extension

--- unstructured_ingest/v2/pipeline/steps/stage.py
+++ unstructured_ingest/v2/pipeline/steps/stage.py
@@ -39,11 +39,13 @@ class UploadStageStep(PipelineStep):
         self, fn: Callable, path: str, file_data_path: str
     ) -> UploadStageStepResponse:
         path = Path(path)
+        # Maintain extension
+        output_filename = f"{self.get_hash(extras=[path.name])}{path.suffix}"
         fn_kwargs = {
             "elements_filepath": path,
             "file_data": FileData.from_file(path=file_data_path),
             "output_dir": self.cache_dir,
-            "output_filename": self.get_hash(extras=[path.name]),
+            "output_filename": output_filename,
         }
         if not asyncio.iscoroutinefunction(fn):
             staged_output_path = fn(**fn_kwargs)
