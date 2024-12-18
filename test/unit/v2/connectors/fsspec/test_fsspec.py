dc8f9c07da3b06421ce7fe2d1788cd8c11eafa5d
Marek Połom <marek.polom@deepsense.ai>
2024-12-18 09:43:33 +0100

    Added new test case; use parametrize for similar test cases

--- test/unit/v2/connectors/fsspec/test_fsspec.py
+++ test/unit/v2/connectors/fsspec/test_fsspec.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pytest
+
 from unstructured_ingest.v2.interfaces import FileData, SourceIdentifiers
 from unstructured_ingest.v2.processes.connectors.fsspec.fsspec import (
     FsspecAccessConfig,
@@ -9,82 +11,77 @@ from unstructured_ingest.v2.processes.connectors.fsspec.fsspec import (
 )
 
 
-def test_get_upload_path_with_source_identifiers():
-    upload_config = FsspecUploaderConfig(remote_url="s3://my-bucket")
-    connection_config = FsspecConnectionConfig(access_config=FsspecAccessConfig())
-    uploader = FsspecUploader(
-        connection_config=connection_config,
-        upload_config=upload_config,
-    )
-    file_data = FileData(
-        identifier="123",
-        source_identifiers=SourceIdentifiers("path/to/file.txt", "/full/path/to/file.txt"),
-        local_download_path=None,
-        additional_metadata={},
-        connector_type="fsspec",
-        display_name="file.txt",
-        metadata={},
-    )
-
-    expected_path = Path("my-bucket/full/path/to/file.txt.json")
-    assert uploader.get_upload_path(file_data) == expected_path
-
-
-def test_get_upload_path_with_source_identifiers_rel_path():
-    upload_config = FsspecUploaderConfig(remote_url="s3://my-bucket")
-    connection_config = FsspecConnectionConfig(access_config=FsspecAccessConfig())
-    uploader = FsspecUploader(
-        connection_config=connection_config,
-        upload_config=upload_config,
-    )
-    file_data = FileData(
-        identifier="123",
-        source_identifiers=SourceIdentifiers(
-            "path/to/file.txt", "/full/path/to/file.txt", "rel/path/to/file.txt"
+@pytest.mark.parametrize(
+    ("file_data", "expected_path"),
+    [
+        (
+            FileData(
+                identifier="123",
+                source_identifiers=SourceIdentifiers("path/to/file.txt", "/full/path/to/file.txt"),
+                local_download_path=None,
+                additional_metadata={},
+                connector_type="fsspec",
+                display_name="file.txt",
+                metadata={},
+            ),
+            Path("my-bucket/full/path/to/file.txt.json"),
         ),
-        local_download_path=None,
-        additional_metadata={},
-        connector_type="fsspec",
-        display_name="file.txt",
-        metadata={},
-    )
-
-    expected_path = Path("my-bucket/rel/path/to/file.txt.json")
-    assert uploader.get_upload_path(file_data) == expected_path
-
-
-def test_get_upload_path_with_local_download_path():
+        (
+            FileData(
+                identifier="123",
+                source_identifiers=SourceIdentifiers(
+                    "path/to/file.txt", "/full/path/to/file.txt", "rel/path/to/file.txt"
+                ),
+                local_download_path=None,
+                additional_metadata={},
+                connector_type="fsspec",
+                display_name="file.txt",
+                metadata={},
+            ),
+            Path("my-bucket/rel/path/to/file.txt.json"),
+        ),
+        (
+            FileData(
+                identifier="123",
+                local_download_path="local/path/to/file.txt",
+                additional_metadata={},
+                connector_type="fsspec",
+                display_name="file.txt",
+                metadata={},
+            ),
+            Path("my-bucket/file.txt.json"),
+        ),
+        (
+            FileData(
+                identifier="123",
+                additional_metadata={},
+                connector_type="fsspec",
+                display_name="file.txt",
+                metadata={},
+            ),
+            Path("my-bucket/123.json"),
+        ),
+        (
+            FileData(
+                identifier="123",
+                source_identifiers=SourceIdentifiers(
+                    "path/to/file name with spaces.txt", "/full/path/to/file name with spaces.txt"
+                ),
+                local_download_path=None,
+                additional_metadata={},
+                connector_type="fsspec",
+                display_name="file name with spaces.txt",
+                metadata={},
+            ),
+            Path("my-bucket/full/path/to/file name with spaces.txt.json"),
+        ),
+    ],
+)
+def test_get_upload_path(file_data, expected_path):
     upload_config = FsspecUploaderConfig(remote_url="s3://my-bucket")
     connection_config = FsspecConnectionConfig(access_config=FsspecAccessConfig())
     uploader = FsspecUploader(
         connection_config=connection_config,
         upload_config=upload_config,
     )
-    file_data = FileData(
-        identifier="123",
-        local_download_path="local/path/to/file.txt",
-        additional_metadata={},
-        connector_type="fsspec",
-        display_name="file.txt",
-        metadata={},
-    )
-    expected_path = Path("my-bucket/file.txt.json")
-    assert uploader.get_upload_path(file_data) == expected_path
-
-
-def test_get_upload_path_with_identifier():
-    upload_config = FsspecUploaderConfig(remote_url="s3://my-bucket")
-    connection_config = FsspecConnectionConfig(access_config=FsspecAccessConfig())
-    uploader = FsspecUploader(
-        connection_config=connection_config,
-        upload_config=upload_config,
-    )
-    file_data = FileData(
-        identifier="123",
-        additional_metadata={},
-        connector_type="fsspec",
-        display_name="file.txt",
-        metadata={},
-    )
-    expected_path = Path("my-bucket/123.json")
     assert uploader.get_upload_path(file_data) == expected_path
