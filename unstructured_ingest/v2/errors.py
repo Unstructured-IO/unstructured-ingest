a4eb11cfd5f51e0b2e123cf19da0a052e4a182e7
Roman Isecke <roman@unstructured.io>
2024-12-19 13:40:50 -0500

    Add error handling to openai embedder

--- unstructured_ingest/v2/errors.py
+++ unstructured_ingest/v2/errors.py
@@ -2,5 +2,17 @@ class UserError(Exception):
     pass
 
 
+class UserAuthError(UserError):
+    pass
+
+
+class RateLimitError(UserError):
+    pass
+
+
+class QuotaError(UserError):
+    pass
+
+
 class ProviderError(Exception):
     pass
