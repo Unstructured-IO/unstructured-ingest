7f921a1545a6b15dfcb438b8482ed132e4531a54
Roman Isecke <roman@unstructured.io>
2024-12-19 13:40:50 -0500

    Add error handling to openai embedder

--- unstructured_ingest/embed/openai.py
+++ unstructured_ingest/embed/openai.py
@@ -4,7 +4,9 @@ from typing import TYPE_CHECKING
 from pydantic import Field, SecretStr
 
 from unstructured_ingest.embed.interfaces import BaseEmbeddingEncoder, EmbeddingConfig
+from unstructured_ingest.logger import logger
 from unstructured_ingest.utils.dep_check import requires_dependencies
+from unstructured_ingest.v2.errors import ProviderError, RateLimitError, UserAuthError, UserError
 
 if TYPE_CHECKING:
     from openai import OpenAI
@@ -25,9 +27,33 @@ class OpenAIEmbeddingConfig(EmbeddingConfig):
 class OpenAIEmbeddingEncoder(BaseEmbeddingEncoder):
     config: OpenAIEmbeddingConfig
 
+    def handle_error(self, e: Exception):
+        # https://platform.openai.com/docs/guides/error-codes/api-errors
+        from openai import APIStatusError
+
+        if not isinstance(e, APIStatusError):
+            logger.error(f"unhandled exception from openai: {e}", exc_info=True)
+            raise e
+        if 400 <= e.status_code < 500:
+            # user error
+            if e.status_code == 401:
+                raise UserAuthError(e.message)
+            if e.status_code == 429:
+                # 429 indicates rate limit exceeded and quote exceeded
+                raise RateLimitError(e.message)
+            raise UserError(e.message)
+        if e.status_code >= 500:
+            raise ProviderError(e.message)
+        logger.error(f"unhandled exception from openai: {e}", exc_info=True)
+        raise e
+
     def embed_query(self, query: str) -> list[float]:
+
         client = self.config.get_client()
-        response = client.embeddings.create(input=query, model=self.config.embedder_model_name)
+        try:
+            response = client.embeddings.create(input=query, model=self.config.embedder_model_name)
+        except Exception as e:
+            self.handle_error(e=e)
         return response.data[0].embedding
 
     def embed_documents(self, elements: list[dict]) -> list[dict]:
