a2b2242672cb3bda225f5924e770346d587b6cf2
Roman Isecke <roman@unstructured.io>
2024-12-19 15:14:34 -0500

    Add error handling to octoai embedder

--- unstructured_ingest/embed/octoai.py
+++ unstructured_ingest/embed/octoai.py
@@ -4,7 +4,9 @@ from typing import TYPE_CHECKING
 from pydantic import Field, SecretStr
 
 from unstructured_ingest.embed.interfaces import BaseEmbeddingEncoder, EmbeddingConfig
+from unstructured_ingest.logger import logger
 from unstructured_ingest.utils.dep_check import requires_dependencies
+from unstructured_ingest.v2.errors import ProviderError, RateLimitError, UserAuthError, UserError
 
 if TYPE_CHECKING:
     from openai import OpenAI
@@ -30,12 +32,41 @@ class OctoAiEmbeddingConfig(EmbeddingConfig):
 class OctoAIEmbeddingEncoder(BaseEmbeddingEncoder):
     config: OctoAiEmbeddingConfig
 
+    def handle_error(self, e: Exception):
+        # https://platform.openai.com/docs/guides/error-codes/api-errors
+        from openai import APIStatusError
+
+        if not isinstance(e, APIStatusError):
+            logger.error(f"unhandled exception from openai: {e}", exc_info=True)
+            raise e
+        if 400 <= e.status_code < 500:
+            # user error
+            if e.status_code == 401:
+                raise UserAuthError(e.message)
+            if e.status_code == 429:
+                # 429 indicates rate limit exceeded and quote exceeded
+                raise RateLimitError(e.message)
+            raise UserError(e.message)
+        if e.status_code >= 500:
+            raise ProviderError(e.message)
+        logger.error(f"unhandled exception from openai: {e}", exc_info=True)
+        raise e
+
     def embed_query(self, query: str):
-        client = self.config.get_client()
-        response = client.embeddings.create(input=query, model=self.config.embedder_model_name)
+        try:
+            client = self.config.get_client()
+            response = client.embeddings.create(input=query, model=self.config.embedder_model_name)
+        except Exception as e:
+            self.handle_error(e=e)
         return response.data[0].embedding
 
     def embed_documents(self, elements: list[dict]) -> list[dict]:
-        embeddings = [self.embed_query(e.get("text", "")) for e in elements]
+        texts = [e.get("text", "") for e in elements]
+        try:
+            client = self.config.get_client()
+            response = client.embeddings.create(input=texts, model=self.config.embedder_model_name)
+        except Exception as e:
+            self.handle_error(e=e)
+        embeddings = [data.embedding for data in response.data]
         elements_with_embeddings = self._add_embeddings_to_elements(elements, embeddings)
         return elements_with_embeddings
