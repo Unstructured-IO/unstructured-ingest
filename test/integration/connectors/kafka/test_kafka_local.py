c6304e3f42b2c543d27984b4a2d49d07c5d761e6
Michal Martyniak <michal.martyniak@deepsense.ai>
2025-01-03 14:38:27 +0100

    revert changes in kafka local

--- test/integration/connectors/kafka/test_kafka_local.py
+++ test/integration/connectors/kafka/test_kafka_local.py
@@ -1,5 +1,4 @@
 import json
-import os
 import tempfile
 from pathlib import Path
 
@@ -23,7 +22,6 @@ from test.integration.connectors.utils.validation.source import (
     SourceValidationConfigs,
     source_connector_validation,
 )
-from test.integration.utils import requires_env
 from unstructured_ingest.error import DestinationConnectionError, SourceConnectionError
 from unstructured_ingest.v2.interfaces import FileData, SourceIdentifiers
 from unstructured_ingest.v2.logger import logger
@@ -92,92 +90,6 @@ async def test_kafka_source_local(kafka_seed_topic: str):
             downloader=downloader,
             configs=SourceValidationConfigs(
                 test_id="kafka-local", expected_num_files=5, validate_downloaded_files=True
-            ),
-        )
-
-
-@pytest.fixture
-def kafka_seed_topic_cloud(expected_messages: int = 5) -> int:
-    conf = {
-        "bootstrap.servers": os.environ["KAFKA_BOOTSTRAP_SERVER"],
-        "sasl.username": os.environ["KAFKA_API_KEY"],
-        "sasl.password": os.environ["KAFKA_SECRET"],
-        "sasl.mechanism": "PLAIN",
-        "security.protocol": "SASL_SSL",
-    }
-    admin_client = AdminClient(conf)
-    try:
-        res = admin_client.delete_topics([TOPIC], operation_timeout=10)
-        for topic, f in res.items():
-            f.result()
-            print(f"Topic {topic} removed")
-            wait_for_topic(TOPIC, 5, 1, False, admin_client)
-    except Exception:
-        pass
-
-    cluster_meta = admin_client.list_topics()
-    current_topics = [topic for topic in cluster_meta.topics if topic != "__consumer_offsets"]
-
-    assert TOPIC not in current_topics, f"Topic {TOPIC} shouldn't exist"
-
-    # Kafka Cloud allows to use replication_factor=1 only for Dedicated clusters.
-    topic_obj = NewTopic(TOPIC, num_partitions=1, replication_factor=3)
-
-    res = admin_client.create_topics([topic_obj], operation_timeout=10, validate_only=False)
-    for topic, f in res.items():
-        f.result()
-
-    producer = Producer(conf)
-    for i in range(expected_messages):
-        message = f"This is some text for message {i}"
-        producer.produce(topic=TOPIC, value=message)
-    producer.flush(timeout=10)
-    return expected_messages
-
-
-@pytest.mark.asyncio
-@pytest.mark.tags(CONNECTOR_TYPE, SOURCE_TAG)
-@requires_env("KAFKA_API_KEY", "KAFKA_SECRET", "KAFKA_BOOTSTRAP_SERVER")
-async def test_kafka_source_cloud(kafka_seed_topic_cloud: int):
-    """
-    In order to have this test succeed, you need to create cluster on Confluent Cloud,
-    and create the API key with admin privileges. By default, user account keys have it.
-    """
-
-    expected_messages = kafka_seed_topic_cloud
-
-    connection_config = CloudKafkaConnectionConfig(
-        bootstrap_server=os.environ["KAFKA_BOOTSTRAP_SERVER"],
-        port=9092,
-        access_config=CloudKafkaAccessConfig(
-            kafka_api_key=os.environ["KAFKA_API_KEY"],
-            secret=os.environ["KAFKA_SECRET"],
-        ),
-    )
-
-    with tempfile.TemporaryDirectory() as tempdir:
-        tempdir_path = Path(tempdir)
-        download_config = CloudKafkaDownloaderConfig(download_dir=tempdir_path)
-        indexer = CloudKafkaIndexer(
-            connection_config=connection_config,
-            index_config=CloudKafkaIndexerConfig(
-                topic=TOPIC,
-                num_messages_to_consume=expected_messages,
-            ),
-        )
-        downloader = CloudKafkaDownloader(
-            connection_config=connection_config, download_config=download_config
-        )
-        indexer.precheck()
-        await source_connector_validation(
-            indexer=indexer,
-            downloader=downloader,
-            configs=ValidationConfigs(
-                test_id="kafka",
-                exclude_fields_extend=["connector_type"],
-                expected_num_files=expected_messages,
-                validate_downloaded_files=True,
-                validate_file_data=True,
             ),
         )
 
