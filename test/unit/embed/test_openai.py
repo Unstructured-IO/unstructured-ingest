0c58aede37f65076cb8e854b8cccaf0991c945d1
Roman Isecke <roman@unstructured.io>
2025-01-30 09:55:43 -0500

    check batch support in openai unit test

--- test/unit/embed/test_openai.py
+++ test/unit/embed/test_openai.py
@@ -3,10 +3,10 @@ from unstructured_ingest.embed.openai import OpenAIEmbeddingConfig, OpenAIEmbedd
 
 def test_embed_documents_does_not_break_element_to_dict(mocker):
     # Mocked client with the desired behavior for embed_documents
-    raw_elements = [{"text": f"This is sentence {i + 1}"} for i in range(2)]
+    raw_elements = [{"text": f"This is sentence {i + 1}"} for i in range(4)]
     mock_response = mocker.MagicMock()
     mock_response_data = []
-    for i in range(len(raw_elements)):
+    for i in range(2):
         mock_response_d = mocker.MagicMock()
         mock_response_d.embedding = [1, 2]
         mock_response_data.append(mock_response_d)
@@ -17,11 +17,12 @@ def test_embed_documents_does_not_break_element_to_dict(mocker):
     # Mock get_client to return our mock_client
     mocker.patch.object(OpenAIEmbeddingConfig, "get_client", return_value=mock_client)
 
-    encoder = OpenAIEmbeddingEncoder(config=OpenAIEmbeddingConfig(api_key="api_key"))
+    encoder = OpenAIEmbeddingEncoder(config=OpenAIEmbeddingConfig(api_key="api_key", batch_size=2))
 
     elements = encoder.embed_documents(
         elements=raw_elements,
     )
-    assert len(elements) == 2
+    assert len(elements) == 4
     assert elements[0]["text"] == "This is sentence 1"
     assert elements[1]["text"] == "This is sentence 2"
+    assert mock_client.embeddings.create.call_count == 2
