6b65d7c906bc37580f088cf5e56c55fb71b66291
Nathan Van Gheem <vangheem@gmail.com>
2025-01-31 16:54:45 -0500

    fix onedrive connector

--- unstructured_ingest/v2/interfaces/indexer.py
+++ unstructured_ingest/v2/interfaces/indexer.py
@@ -22,9 +22,8 @@ class Indexer(BaseProcess, BaseConnector, ABC):
     def is_async(self) -> bool:
         return False
 
-    @abstractmethod
     def run(self, **kwargs: Any) -> Generator[FileData, None, None]:
-        pass
+        raise NotImplementedError()
 
     async def run_async(self, **kwargs: Any) -> AsyncGenerator[FileData, None]:
         raise NotImplementedError()
