dcb905d7d852fd6bd9301731f274ed8040e141d1
David Potter <potterdavidm@gmail.com>
2025-02-02 17:21:03 -0800

    switch pipeline qualification to type

--- unstructured_ingest/v2/processes/connectors/sharepoint.py
+++ unstructured_ingest/v2/processes/connectors/sharepoint.py
@@ -35,6 +35,16 @@ from unstructured_ingest.v2.processes.connector_registry import (
     DestinationRegistryEntry,
     SourceRegistryEntry,
 )
+from unstructured_ingest.v2.processes.connectors.onedrive import (
+    OnedriveAccessConfig,
+    MAX_BYTES_SIZE,
+    OnedriveConnectionConfig,
+    OnedriveDownloader,
+    OnedriveDownloaderConfig,
+    OnedriveIndexer,
+    OnedriveIndexerConfig,
+)
+
 
 if TYPE_CHECKING:
     from office365.graph_client import GraphClient
@@ -42,15 +52,15 @@ if TYPE_CHECKING:
     from office365.onedrive.drives.drive import Drive
 
 CONNECTOR_TYPE = "sharepoint"
-MAX_BYTES_SIZE = 512_000_000
 
 
-class SharepointAccessConfig(AccessConfig):
+class SharepointAccessConfig(OnedriveAccessConfig):
     client_cred: str = Field(description="Microsoft App client secret")
+    pass
 
 
-class SharepointConnectionConfig(ConnectionConfig):
-    client_id: str = Field(description="Microsoft app client ID")
+class SharepointConnectionConfig(OnedriveConnectionConfig):
+    # client_id: str = Field(description="Microsoft app client ID")
     site: str = Field(
         description="Sharepoint site url. Process either base url e.g \
                     https://[tenant].sharepoint.com  or relative sites \
@@ -59,151 +69,152 @@ class SharepointConnectionConfig(ConnectionConfig):
                     https://[tenant]-admin.sharepoint.com.\
                     This requires the app to be registered at a tenant level"
     )
-    user_pname: str = Field(description="User principal name, usually is your Azure AD email.")
-    tenant: str = Field(
-        repr=False, description="ID or domain name associated with your Azure AD instance"
-    )
-    authority_url: Optional[str] = Field(
-        repr=False,
-        default="https://login.microsoftonline.com",
-        examples=["https://login.microsoftonline.com"],
-        description="Authentication token provider for Microsoft apps",
-    )
-    access_config: Secret[SharepointAccessConfig]
+    # user_pname: str = Field(description="User principal name, usually is your Azure AD email.")
+    # tenant: str = Field(
+    #     repr=False, description="ID or domain name associated with your Azure AD instance"
+    # )
+    # authority_url: Optional[str] = Field(
+    #     repr=False,
+    #     default="https://login.microsoftonline.com",
+    #     examples=["https://login.microsoftonline.com"],
+    #     description="Authentication token provider for Microsoft apps",
+    # )
+    # access_config: Secret[SharepointAccessConfig]
 
-    def get_drive(self) -> "Drive":
-        client = self.get_client()
-        drive = client.users[self.user_pname].drive
-        return drive
+    # def get_drive(self) -> "Drive":
+    #     client = self.get_client()
+    #     drive = client.users[self.user_pname].drive
+    #     return drive
 
-    @requires_dependencies(["msal"], extras="sharepoint")
-    def get_token(self):
-        from msal import ConfidentialClientApplication
+    # @requires_dependencies(["msal"], extras="sharepoint")
+    # def get_token(self):
+    #     from msal import ConfidentialClientApplication
 
-        try:
-            app = ConfidentialClientApplication(
-                authority=f"{self.authority_url}/{self.tenant}",
-                client_id=self.client_id,
-                client_credential=self.access_config.get_secret_value().client_cred,
-            )
-            token = app.acquire_token_for_client(scopes=["https://graph.microsoft.com/.default"])
-        except ValueError as exc:
-            logger.error("Couldn't set up credentials for Sharepoint")
-            raise exc
-        if "error" in token:
-            raise SourceConnectionNetworkError(
-                "failed to fetch token, {}: {}".format(token["error"], token["error_description"])
-            )
-        return token
+    #     try:
+    #         app = ConfidentialClientApplication(
+    #             authority=f"{self.authority_url}/{self.tenant}",
+    #             client_id=self.client_id,
+    #             client_credential=self.access_config.get_secret_value().client_cred,
+    #         )
+    #         token = app.acquire_token_for_client(scopes=["https://graph.microsoft.com/.default"])
+    #     except ValueError as exc:
+    #         logger.error("Couldn't set up credentials for Sharepoint")
+    #         raise exc
+    #     if "error" in token:
+    #         raise SourceConnectionNetworkError(
+    #             "failed to fetch token, {}: {}".format(token["error"], token["error_description"])
+    #         )
+    #     return token
 
-    @requires_dependencies(["office365"], extras="sharepoint")
-    def get_client(self) -> "GraphClient":
-        from office365.graph_client import GraphClient
+    # @requires_dependencies(["office365"], extras="sharepoint")
+    # def get_client(self) -> "GraphClient":
+    #     from office365.graph_client import GraphClient
 
-        client = GraphClient(self.get_token)
-        return client
+    #     client = GraphClient(self.get_token)
+        # return client
 
 
-class SharepointIndexerConfig(IndexerConfig):
-    path: Optional[str] = Field(default="")
-    recursive: bool = False
+class SharepointIndexerConfig(OnedriveIndexerConfig):
+    # path: Optional[str] = Field(default="")
+    # recursive: bool = False
+    pass
 
 
 @dataclass
-class SharepointIndexer(Indexer):
+class SharepointIndexer(OnedriveIndexer):
     connection_config: SharepointConnectionConfig
     index_config: SharepointIndexerConfig
 
-    def precheck(self) -> None:
-        try:
-            token_resp: dict = self.connection_config.get_token()
-            if error := token_resp.get("error"):
-                raise SourceConnectionError(
-                    "{} ({})".format(error, token_resp.get("error_description"))
-                )
-        except Exception as e:
-            logger.error(f"failed to validate connection: {e}", exc_info=True)
-            raise SourceConnectionError(f"failed to validate connection: {e}")
+    # def precheck(self) -> None:
+    #     try:
+    #         token_resp: dict = self.connection_config.get_token()
+    #         if error := token_resp.get("error"):
+    #             raise SourceConnectionError(
+    #                 "{} ({})".format(error, token_resp.get("error_description"))
+    #             )
+    #     except Exception as e:
+    #         logger.error(f"failed to validate connection: {e}", exc_info=True)
+    #         raise SourceConnectionError(f"failed to validate connection: {e}")
 
-    def list_objects_sync(self, folder: DriveItem, recursive: bool) -> list["DriveItem"]:
-        drive_items = folder.children.get().execute_query()
-        files = [d for d in drive_items if d.is_file]
-        if not recursive:
-            return files
+    # def list_objects_sync(self, folder: DriveItem, recursive: bool) -> list["DriveItem"]:
+    #     drive_items = folder.children.get().execute_query()
+    #     files = [d for d in drive_items if d.is_file]
+    #     if not recursive:
+    #         return files
 
-        folders = [d for d in drive_items if d.is_folder]
-        for f in folders:
-            files.extend(self.list_objects_sync(f, recursive))
-        return files
+    #     folders = [d for d in drive_items if d.is_folder]
+    #     for f in folders:
+    #         files.extend(self.list_objects_sync(f, recursive))
+    #     return files
 
-    async def list_objects(self, folder: "DriveItem", recursive: bool) -> list["DriveItem"]:
-        return await asyncio.to_thread(self.list_objects_sync, folder, recursive)
+    # async def list_objects(self, folder: "DriveItem", recursive: bool) -> list["DriveItem"]:
+    #     return await asyncio.to_thread(self.list_objects_sync, folder, recursive)
 
-    def get_root_sync(self, client: "GraphClient") -> "DriveItem":
-        root = client.users[self.connection_config.user_pname].drive.get().execute_query().root
-        if fpath := self.index_config.path:
-            root = root.get_by_path(fpath).get().execute_query()
-            if root is None or not root.is_folder:
-                raise ValueError(f"Unable to find directory, given: {fpath}")
-        return root
+    # def get_root_sync(self, client: "GraphClient") -> "DriveItem":
+    #     root = client.users[self.connection_config.user_pname].drive.get().execute_query().root
+    #     if fpath := self.index_config.path:
+    #         root = root.get_by_path(fpath).get().execute_query()
+    #         if root is None or not root.is_folder:
+    #             raise ValueError(f"Unable to find directory, given: {fpath}")
+    #     return root
 
-    async def get_root(self, client: "GraphClient") -> "DriveItem":
-        return await asyncio.to_thread(self.get_root_sync, client)
+    # async def get_root(self, client: "GraphClient") -> "DriveItem":
+    #     return await asyncio.to_thread(self.get_root_sync, client)
 
-    def get_properties_sync(self, drive_item: "DriveItem") -> dict:
-        properties = drive_item.properties
-        filtered_properties = {}
-        for k, v in properties.items():
-            try:
-                json.dumps(v)
-                filtered_properties[k] = v
-            except TypeError:
-                pass
-        return filtered_properties
+    # def get_properties_sync(self, drive_item: "DriveItem") -> dict:
+    #     properties = drive_item.properties
+    #     filtered_properties = {}
+    #     for k, v in properties.items():
+    #         try:
+    #             json.dumps(v)
+    #             filtered_properties[k] = v
+    #         except TypeError:
+    #             pass
+    #     return filtered_properties
 
-    async def get_properties(self, drive_item: "DriveItem") -> dict:
-        return await asyncio.to_thread(self.get_properties_sync, drive_item)
+    # async def get_properties(self, drive_item: "DriveItem") -> dict:
+    #     return await asyncio.to_thread(self.get_properties_sync, drive_item)
 
-    def drive_item_to_file_data_sync(self, drive_item: "DriveItem") -> FileData:
-        file_path = drive_item.parent_reference.path.split(":")[-1]
-        file_path = file_path[1:] if file_path and file_path[0] == "/" else file_path
-        filename = drive_item.name
-        server_path = file_path + "/" + filename
-        rel_path = server_path.replace(self.index_config.path, "").lstrip("/")
-        date_modified_dt = (
-            parser.parse(str(drive_item.last_modified_datetime))
-            if drive_item.last_modified_datetime
-            else None
-        )
-        date_created_at = (
-            parser.parse(str(drive_item.created_datetime)) if drive_item.created_datetime else None
-        )
-        return FileData(
-            identifier=drive_item.id,
-            connector_type=CONNECTOR_TYPE,
-            source_identifiers=SourceIdentifiers(
-                fullpath=server_path, filename=drive_item.name, rel_path=rel_path
-            ),
-            metadata=FileDataSourceMetadata(
-                url=drive_item.parent_reference.path + "/" + drive_item.name,
-                version=drive_item.etag,
-                date_modified=str(date_modified_dt.timestamp()) if date_modified_dt else None,
-                date_created=str(date_created_at.timestamp()) if date_created_at else None,
-                date_processed=str(time()),
-                record_locator={
-                    "user_pname": self.connection_config.user_pname,
-                    "server_relative_path": server_path,
-                },
-            ),
-            additional_metadata=self.get_properties_sync(drive_item=drive_item),
-        )
+    # def drive_item_to_file_data_sync(self, drive_item: "DriveItem") -> FileData:
+    #     file_path = drive_item.parent_reference.path.split(":")[-1]
+    #     file_path = file_path[1:] if file_path and file_path[0] == "/" else file_path
+    #     filename = drive_item.name
+    #     server_path = file_path + "/" + filename
+    #     rel_path = server_path.replace(self.index_config.path, "").lstrip("/")
+    #     date_modified_dt = (
+    #         parser.parse(str(drive_item.last_modified_datetime))
+    #         if drive_item.last_modified_datetime
+    #         else None
+    #     )
+    #     date_created_at = (
+    #         parser.parse(str(drive_item.created_datetime)) if drive_item.created_datetime else None
+    #     )
+    #     return FileData(
+    #         identifier=drive_item.id,
+    #         connector_type=CONNECTOR_TYPE,
+    #         source_identifiers=SourceIdentifiers(
+    #             fullpath=server_path, filename=drive_item.name, rel_path=rel_path
+    #         ),
+    #         metadata=FileDataSourceMetadata(
+    #             url=drive_item.parent_reference.path + "/" + drive_item.name,
+    #             version=drive_item.etag,
+    #             date_modified=str(date_modified_dt.timestamp()) if date_modified_dt else None,
+    #             date_created=str(date_created_at.timestamp()) if date_created_at else None,
+    #             date_processed=str(time()),
+    #             record_locator={
+    #                 "user_pname": self.connection_config.user_pname,
+    #                 "server_relative_path": server_path,
+    #             },
+    #         ),
+    #         additional_metadata=self.get_properties_sync(drive_item=drive_item),
+    #     )
 
-    async def drive_item_to_file_data(self, drive_item: "DriveItem") -> FileData:
-        # Offload the file data creation if it's not guaranteed async
-        return await asyncio.to_thread(self.drive_item_to_file_data_sync, drive_item)
+    # async def drive_item_to_file_data(self, drive_item: "DriveItem") -> FileData:
+    #     # Offload the file data creation if it's not guaranteed async
+    #     return await asyncio.to_thread(self.drive_item_to_file_data_sync, drive_item)
 
-    def is_async(self) -> bool:
-        return True
+    # def is_async(self) -> bool:
+    #     return True
     
     @requires_dependencies(["office365"], extras="onedrive")
     async def run_async(self, **kwargs: Any) -> AsyncIterator[FileData]:
@@ -229,12 +240,12 @@ class SharepointIndexer(Indexer):
             yield file_data
 
 
-class SharepointDownloaderConfig(DownloaderConfig):
+class SharepointDownloaderConfig(OnedriveDownloaderConfig):
     pass
 
 
 @dataclass
-class SharepointDownloader(Downloader):
+class SharepointDownloader(OnedriveDownloader):
     connection_config: SharepointConnectionConfig
     download_config: SharepointDownloaderConfig
 
