22939b5c422eee65b68925c9508526bf2c811586
Marek Połom <marek.polom@deepsense.ai>
2025-02-10 14:30:26 +0100

    Fix imports

--- test/unit/v2/connectors/motherduck/test_base.py
+++ test/unit/v2/connectors/motherduck/test_base.py
@@ -3,11 +3,10 @@ from pathlib import Path
 import pytest
 from pytest_mock import MockerFixture
 
-from unstructured_ingest.v2.processes.connectors.duckdb.base import BaseDuckDBUploadStager
 from unstructured_ingest.v2.interfaces import FileData
 from unstructured_ingest.v2.interfaces.file_data import SourceIdentifiers
 from unstructured_ingest.v2.interfaces.upload_stager import UploadStagerConfig
-
+from unstructured_ingest.v2.processes.connectors.duckdb.base import BaseDuckDBUploadStager
 
 
 @pytest.fixture
