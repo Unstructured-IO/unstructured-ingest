02ee551a4b660bfeac17fe0be028f2144735e8d9
nikpocuca <nikpocuca@users.noreply.github.com>
2025-02-17 11:09:39 -0500

    linted

--- test/integration/connectors/test_zendesk.py
+++ test/integration/connectors/test_zendesk.py
@@ -7,7 +7,7 @@ from typing import Optional
 import numpy as np
 import pytest
 
-from zenpy import Zenpy, ZenpyException 
+from zenpy import Zenpy, ZenpyException
 
 from test.integration.connectors.utils.validation.source import (
     SourceValidationConfigs,
@@ -17,48 +17,50 @@ from test.integration.connectors.utils.validation.source import (
 from test.integration.connectors.utils.constants import DESTINATION_TAG, NOSQL_TAG
 from test.integration.utils import requires_env
 from unstructured_ingest.v2.interfaces.file_data import FileData, SourceIdentifiers
-from unstructured_ingest.v2.processes.connectors.zendesk import (CONNECTOR_TYPE as ZENDESK_CONNECTOR_TYPE)
+from unstructured_ingest.v2.processes.connectors.zendesk import (
+    CONNECTOR_TYPE as ZENDESK_CONNECTOR_TYPE,
+)
 
 
 from unstructured_ingest.v2.processes.connectors.zendesk import (
     ZendeskConnectionConfig,
     ZendeskAccessConfig,
-    ZendeskIndexerConfig, 
-    ZendeskIndexer, 
-    ZendeskDownloaderConfig, 
-    ZendeskDownloader
+    ZendeskIndexerConfig,
+    ZendeskIndexer,
+    ZendeskDownloaderConfig,
+    ZendeskDownloader,
 )
 
 
 async def zendesk_source_test(
-        tmp_path: Path, 
-        token: Optional[str] = None,    
-        endpoint: Optional[str] = None, 
-        email: Optional[str] = None, 
-        subdomain: Optional[str] = None, 
-    ): 
-    
+    tmp_path: Path,
+    token: Optional[str] = None,
+    endpoint: Optional[str] = None,
+    email: Optional[str] = None,
+    subdomain: Optional[str] = None,
+):
+
     access_config = ZendeskAccessConfig(api_token=token)
     connection_config = ZendeskConnectionConfig(
-        sub_domain = subdomain,
-        email=email, 
-        endpoint=endpoint,
-        access_config = access_config)
-    
+        sub_domain=subdomain, email=email, endpoint=endpoint, access_config=access_config
+    )
+
     index_config = ZendeskIndexerConfig(batch_size=1)
 
     indexer = ZendeskIndexer(
-        connection_config=connection_config, 
+        connection_config=connection_config,
         index_config=index_config,
-        connector_type = "zendesk",
+        connector_type="zendesk",
     )
-    
-    # handle downloader. 
+
+    # handle downloader.
     download_config = ZendeskDownloaderConfig(download_dir=tmp_path)
 
-    downloader = ZendeskDownloader(connection_config=connection_config,
-                                   download_config=download_config,
-                                   connector_type="zendesk")
+    downloader = ZendeskDownloader(
+        connection_config=connection_config,
+        download_config=download_config,
+        connector_type="zendesk",
+    )
 
     # Run the source connector validation
     await source_connector_validation(
@@ -69,11 +71,14 @@ async def zendesk_source_test(
         ),
     )
 
+
 @pytest.mark.asyncio
 @requires_env("ZENDESK_ENDPOINT", "ZENDESK_TOKEN")
 async def test_zendesk_source(temp_dir):
-    await zendesk_source_test(tmp_path=temp_dir, 
-                        token=os.environ["ZENDESK_TOKEN"],
-                        endpoint=os.environ["ZENDESK_ENDPOINT"],
-                        email="test@unstructured.io",
-                        subdomain="unstructuredhelp")
+    await zendesk_source_test(
+        tmp_path=temp_dir,
+        token=os.environ["ZENDESK_TOKEN"],
+        endpoint=os.environ["ZENDESK_ENDPOINT"],
+        email="test@unstructured.io",
+        subdomain="unstructuredhelp",
+    )
--- unstructured_ingest/v2/processes/connectors/zendesk.py
+++ unstructured_ingest/v2/processes/connectors/zendesk.py
@@ -1,14 +1,10 @@
 from __future__ import annotations
 
-import asyncio
-import json 
 from dataclasses import dataclass
 from pathlib import Path
-from time import time 
-from typing import TYPE_CHECKING, Any, AsyncIterator, Optional, Generator
-import io 
+from time import time
+from typing import TYPE_CHECKING, Any, Generator
 
-from dateutil import parser
 from pydantic import Field, Secret
 import hashlib
 
@@ -27,47 +23,50 @@ from unstructured_ingest.v2.processes.connector_registry import SourceRegistryEn
 from unstructured_ingest.utils.dep_check import requires_dependencies
 from unstructured_ingest.v2.interfaces import (
     AccessConfig,
-    ConnectionConfig, 
-    Downloader, 
-    DownloaderConfig, 
-    DownloadResponse, 
-    FileData, 
-    FileDataSourceMetadata, 
-    Indexer, 
-    IndexerConfig, 
+    ConnectionConfig,
+    Downloader,
+    DownloaderConfig,
+    DownloadResponse,
+    FileData,
+    FileDataSourceMetadata,
+    Indexer,
+    IndexerConfig,
     SourceIdentifiers,
 )
 
 from unstructured_ingest.v2.logger import logger
-from unstructured_ingest.v2.processes.connector_registry import (
-    SourceRegistryEntry
-)
+from unstructured_ingest.v2.processes.connector_registry import SourceRegistryEntry
 
 
-if TYPE_CHECKING: 
+if TYPE_CHECKING:
     from zenpy import Zenpy
     from zenpy.lib.api_objects.chat_objects import Tickets
 
 CONNECTOR_TYPE = "zendesk"
 
+
 class ZendeskAccessConfig(AccessConfig):
-    api_token: str = Field(description="API token for zendesk generated under Apps and Integrations")
+    api_token: str = Field(
+        description="API token for zendesk generated under Apps and Integrations"
+    )
+
 
 class ZendeskConnectionConfig(ConnectionConfig):
     sub_domain: str = Field(description="Subdomain for zendesk site, <sub-domain>.company.com")
-    email: str = Field(description ="Email for zendesk site registered at the subdomain")
+    email: str = Field(description="Email for zendesk site registered at the subdomain")
     access_config: Secret[ZendeskAccessConfig]
 
     @requires_dependencies(["zenpy"], extras="zenpy")
     @contextmanager
     def get_client(self) -> Generator["Zenpy", None, None]:
         import zenpy
+
         access_config = self.access_config.get_secret_value()
-        
+
         options = {
-            'subdomain': self.sub_domain,
-            'email' : self.email, 
-            'token': access_config.api_token,
+            "subdomain": self.sub_domain,
+            "email": self.email,
+            "token": access_config.api_token,
         }
 
         client = zenpy.Zenpy(**options)
@@ -75,7 +74,11 @@ class ZendeskConnectionConfig(ConnectionConfig):
 
 
 class ZendeskIndexerConfig(IndexerConfig):
-    batch_size: int = Field(default="1", description="[NotImplemented]Number of tickets: Currently batching is not supported")
+    batch_size: int = Field(
+        default="1",
+        description="[NotImplemented]Number of tickets: Currently batching is not supported",
+    )
+
 
 @dataclass
 class ZendeskIndexer(Indexer):
@@ -83,40 +86,37 @@ class ZendeskIndexer(Indexer):
     index_config: ZendeskIndexerConfig
     connector_type: str = CONNECTOR_TYPE
 
-
     def precheck(self) -> None:
         """Validates connection to Zendesk api"""
-        try: 
-            # there is no context manager method for Zenpy. 
-            with self.connection_config.get_client() as client: 
+        try:
+            # there is no context manager method for Zenpy.
+            with self.connection_config.get_client() as client:
 
                 if client.users()[:] == []:
                     raise SourceConnectionError(
                         f"users do not exist in zendesk subdomain {self.connection_config.sub_domain}"
-                    ) 
+                    )
 
-        except Exception as e: 
-            logger.error(f'Failed to validate connection to Zendesk: {e}', exc_info=True)
+        except Exception as e:
+            logger.error(f"Failed to validate connection to Zendesk: {e}", exc_info=True)
             raise SourceConnectionError(f"Failed to validate connection: {e}")
 
     def run_async(self, **kwargs):
         return NotImplementedError
-    
+
     def is_async(self) -> bool:
         return False
 
     def _list_tickets(self):
-        with self.connection_config.get_client() as client: 
+        with self.connection_config.get_client() as client:
             tickets = client.tickets()
             return tickets
 
     def _list_comments(self, ticket_generator, ticket_id: int):
         return ticket_generator.comments(ticket=ticket_id)
 
-
     def _generate_fullpath(self, ticket) -> Path:
         return Path(hashlib.sha256(str(ticket.id).encode("utf-8")).hexdigest()[:16] + ".txt")
-
 
     # require dependency zenpy
     def run(self, **kwargs: Any) -> Generator[FileData, None, None]:
@@ -127,62 +127,63 @@ class ZendeskIndexer(Indexer):
 
             metadata = FileDataSourceMetadata(
                 date_processed=str(time()),
-                record_locator={
-                    "id": str(ticket.id),
-                    "subject": str(ticket.subject)
-                },
-            )            
+                record_locator={"id": str(ticket.id), "subject": str(ticket.subject)},
+            )
 
             full_path = self._generate_fullpath(ticket)
 
-            source_identifiers=SourceIdentifiers(filename=full_path.name, fullpath=str(full_path))
-            
+            source_identifiers = SourceIdentifiers(filename=full_path.name, fullpath=str(full_path))
+
             file_data = FileData(
                 identifier=str(ticket.id),
-                connector_type=self.connector_type, 
+                connector_type=self.connector_type,
                 metadata=metadata,
-                source_identifiers=source_identifiers
+                source_identifiers=source_identifiers,
             )
 
             yield file_data
 
-class ZendeskDownloaderConfig(DownloaderConfig):
-    pass 
 
-@dataclass 
+class ZendeskDownloaderConfig(DownloaderConfig):
+    pass
+
+
+@dataclass
 class ZendeskDownloader(Downloader):
     download_config: ZendeskDownloaderConfig
     connection_config: ZendeskConnectionConfig
     connector_type: str = CONNECTOR_TYPE
 
-
     @SourceConnectionError.wrap
     @requires_dependencies(["zenpy"], extras="zenpy")
-    def run(self, file_data: FileData, **kwargs: Any) -> DownloadResponse: 
+    def run(self, file_data: FileData, **kwargs: Any) -> DownloadResponse:
 
         zendesk_filedata: FileData = FileData.cast(file_data=file_data)
 
         with self.connection_config.get_client() as client:
-            
+
             comments = []
-            # each ticket consists of comments, to which I will dump in a txt file. 
+            # each ticket consists of comments, to which I will dump in a txt file.
             first_date = None
             for comment in client.tickets.comments(ticket=zendesk_filedata.identifier):
-                
+
                 if isinstance(comment.created, datetime.datetime):
                     date_created = comment.created.isoformat()
                 else:
                     date_created = str(comment.created)
 
-                if first_date is None: 
+                if first_date is None:
                     first_date = date_created
 
-                comments.append({'comment_id':comment.id,
-                                 'author_id':comment.author_id, 
-                                 'body':comment.body, 
-                                 'num_attachments':len(comment.attachments),
-                                 'date_created': date_created
-                                 })
+                comments.append(
+                    {
+                        "comment_id": comment.id,
+                        "author_id": comment.author_id,
+                        "body": comment.body,
+                        "num_attachments": len(comment.attachments),
+                        "date_created": date_created,
+                    }
+                )
 
             # handle filedata bs
             cast_file_data = FileData.cast(file_data=file_data)
@@ -195,29 +196,29 @@ class ZendeskDownloader(Downloader):
 
             download_path.parent.mkdir(parents=True, exist_ok=True)
 
-            # Write the values to the file    
+            # Write the values to the file
             with open(download_path, "w", encoding="utf8") as f:
-                
+
                 # handle json dump here
-                f.write('ticket\n')
+                f.write("ticket\n")
                 f.write(file_data.identifier)
-                f.write('\n')
+                f.write("\n")
                 f.write(first_date)
-                f.write('\n')
-                for comment in comments: 
-                    f.write('comment')
-                    f.write('\n')
-                    f.write(str(comment['comment_id']))
-                    f.write('\n')
-                    f.write(str(comment['author_id']))
-                    f.write('\n')
-                    f.write(comment['body'])
-                    f.write('\n')
-                    f.write(str(comment['num_attachments']))
-                    f.write('\n')
-                    f.write(comment['date_created'])
-                    f.write('\n')
-        
+                f.write("\n")
+                for comment in comments:
+                    f.write("comment")
+                    f.write("\n")
+                    f.write(str(comment["comment_id"]))
+                    f.write("\n")
+                    f.write(str(comment["author_id"]))
+                    f.write("\n")
+                    f.write(comment["body"])
+                    f.write("\n")
+                    f.write(str(comment["num_attachments"]))
+                    f.write("\n")
+                    f.write(comment["date_created"])
+                    f.write("\n")
+
             # Update metadata
             cast_file_data.metadata.date_created = first_date
 
@@ -225,7 +226,8 @@ class ZendeskDownloader(Downloader):
                 file_data=cast_file_data, download_path=download_path
             )
 
-# create entry 
+
+# create entry
 zendesk_source_entry = SourceRegistryEntry(
     connection_config=ZendeskConnectionConfig,
     indexer_config=ZendeskIndexerConfig,
