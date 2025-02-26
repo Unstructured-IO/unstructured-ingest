156da48123ea569c94f395017cf0d691bd659e5a
Marek Połom <marek.polom@deepsense.ai>
2025-02-26 15:30:33 +0100

    shfmt dropbox e2e test

--- test_e2e/src/dropbox.sh
+++ test_e2e/src/dropbox.sh
@@ -16,11 +16,11 @@ CI=${CI:-"false"}
 # shellcheck disable=SC1091
 source "$SCRIPT_DIR"/cleanup.sh
 function cleanup() {
-  cleanup_dir "$OUTPUT_DIR"
-  cleanup_dir "$WORK_DIR"
-  if [ "$CI" == "true" ]; then
-    cleanup_dir "$DOWNLOAD_DIR"
-  fi
+	cleanup_dir "$OUTPUT_DIR"
+	cleanup_dir "$WORK_DIR"
+	if [ "$CI" == "true" ]; then
+		cleanup_dir "$DOWNLOAD_DIR"
+	fi
 }
 trap cleanup EXIT
 
@@ -36,22 +36,22 @@ trap cleanup EXIT
 
 RUN_SCRIPT=${RUN_SCRIPT:-./unstructured_ingest/main.py}
 PYTHONPATH=${PYTHONPATH:-.} "$RUN_SCRIPT" \
-  dropbox \
-  --api-key "$UNS_PAID_API_KEY" \
-  --partition-by-api \
-  --partition-endpoint "https://api.unstructuredapp.io" \
-  --num-processes "$max_processes" \
-  --download-dir "$DOWNLOAD_DIR" \
-  --metadata-exclude coordinates,filename,file_directory,metadata.data_source.date_processed,metadata.last_modified,metadata.detection_class_prob,metadata.parent_id,metadata.category_depth \
-  --preserve-downloads \
-  --reprocess \
-  --output-dir "$OUTPUT_DIR" \
-  --verbose \
-  --refresh-token "$DROPBOX_REFRESH_TOKEN" \
-  --app_key "$DROPBOX_APP_KEY" \
-  --app_secret "$DROPBOX_APP_SECRET" \
-  --recursive \
-  --remote-url "dropbox://test-input/" \
-  --work-dir "$WORK_DIR"
+	dropbox \
+	--api-key "$UNS_PAID_API_KEY" \
+	--partition-by-api \
+	--partition-endpoint "https://api.unstructuredapp.io" \
+	--num-processes "$max_processes" \
+	--download-dir "$DOWNLOAD_DIR" \
+	--metadata-exclude coordinates,filename,file_directory,metadata.data_source.date_processed,metadata.last_modified,metadata.detection_class_prob,metadata.parent_id,metadata.category_depth \
+	--preserve-downloads \
+	--reprocess \
+	--output-dir "$OUTPUT_DIR" \
+	--verbose \
+	--refresh-token "$DROPBOX_REFRESH_TOKEN" \
+	--app_key "$DROPBOX_APP_KEY" \
+	--app_secret "$DROPBOX_APP_SECRET" \
+	--recursive \
+	--remote-url "dropbox://test-input/" \
+	--work-dir "$WORK_DIR"
 
 "$SCRIPT_DIR"/check-diff-expected-output.py --output-folder-name $OUTPUT_FOLDER_NAME
