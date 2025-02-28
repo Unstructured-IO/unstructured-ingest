a694244c1b9bfac026c3899506216b6d91ffaa9c
Mateusz Kuprowski <mateusz.kuprowski@deepsense.ai>
2025-02-28 15:23:07 +0100

    Ruff fix

--- unstructured_ingest/v2/processes/connectors/fsspec/dropbox.py
+++ unstructured_ingest/v2/processes/connectors/fsspec/dropbox.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass, field
 from time import time
-from typing import TYPE_CHECKING, Optional, Any
+from typing import TYPE_CHECKING, Any, Optional
 
 from pydantic import Field, Secret
 
