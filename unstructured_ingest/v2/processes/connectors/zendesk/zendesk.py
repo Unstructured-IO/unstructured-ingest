d8e3619aa81e9cc723401efafaf50fa08fadc13d
Nik Počuča <25868124+nikpocuca@users.noreply.github.com>
2025-03-04 10:31:46 -0500

    Update unstructured_ingest/v2/processes/connectors/zendesk/zendesk.py
    Co-authored-by: Roman Isecke <136338424+rbiseck3@users.noreply.github.com>

--- unstructured_ingest/v2/processes/connectors/zendesk/zendesk.py
+++ unstructured_ingest/v2/processes/connectors/zendesk/zendesk.py
@@ -114,7 +114,7 @@ class ZendeskIndexerConfig(IndexerConfig):
         default=1,
         description="Number of tickets or articles.",
     )
-    item_type: str = Field(
+    item_type: Literal["tickets", "articles"] = Field(
         default="tickets",
         description="Type of item from zendesk to parse, can only be `tickets` or `articles`.",
     )
