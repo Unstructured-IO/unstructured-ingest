c1620fabc68ba15eb239e27c589f6728adf91f21
Nathan Van Gheem <vangheem@gmail.com>
2025-03-08 11:22:44 -0500

    handle neo4j client error when initializing vector store (#421)
    * handle neo4j client error when initializing vector store
    
    * include text
    
    * .

--- unstructured_ingest/v2/processes/connectors/neo4j.py
+++ unstructured_ingest/v2/processes/connectors/neo4j.py
@@ -154,7 +154,7 @@ class Neo4jUploadStager(UploadStager):
             self._add_entities(element, graph, element_node)
 
             if self._is_chunk(element):
-                for origin_element in format_and_truncate_orig_elements(element):
+                for origin_element in format_and_truncate_orig_elements(element, include_text=True):
                     origin_element_node = self._create_element_node(origin_element)
 
                     graph.add_edge(
@@ -327,13 +327,16 @@ class Neo4jUploader(Uploader):
     async def _create_vector_index(
         self, client: AsyncDriver, dimensions: int, similarity_function: SimilarityFunction
     ) -> None:
+        import neo4j.exceptions
+
         label = Label.CHUNK
         logger.info(
             f"Creating index on nodes labeled '{label.value}' if it does not already exist."
         )
         index_name = f"{label.value.lower()}_vector"
-        await client.execute_query(
-            f"""
+        try:
+            await client.execute_query(
+                f"""
             CREATE VECTOR INDEX {index_name} IF NOT EXISTS
             FOR (n:{label.value}) ON n.embedding
             OPTIONS {{indexConfig: {{
@@ -341,7 +344,12 @@ class Neo4jUploader(Uploader):
                 `vector.dimensions`: {dimensions}}}
             }}
             """
-        )
+            )
+        except neo4j.exceptions.ClientError as e:
+            if e.code == "Neo.ClientError.Schema.EquivalentSchemaRuleAlreadyExists":
+                logger.info(f"Index on nodes labeled '{label.value}' already exists.")
+            else:
+                raise
 
     async def _delete_old_data_if_exists(self, file_data: FileData, client: AsyncDriver) -> None:
         logger.info(f"Deleting old data for the record '{file_data.identifier}' (if present).")
