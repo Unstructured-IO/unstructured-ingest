80be246cec65fafec5ab51607fde3588678a86f2
Chris Trahey <christrahey@gmail.com>
2025-03-24 15:55:49 -0700

    deprecation log line for file_data import from interfaces

--- unstructured_ingest/v2/interfaces/file_data.py
+++ unstructured_ingest/v2/interfaces/file_data.py
@@ -4,3 +4,6 @@ This file has moved to the v2/types/ module.
 The following line exists for backward compatibility.
 """
 from unstructured_ingest.v2.types.file_data import *
+logger = logging.getLogger(__name__)
+logger.warning("Importing file_data.py through interfaces is deprecated. "
+               "Please use unstructured_ingest.v2.types.file_data instead!")
