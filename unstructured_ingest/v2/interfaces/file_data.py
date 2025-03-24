cf1a596d457baf4d56e8c518bb325be8b8966148
Chris Trahey <christrahey@gmail.com>
2025-03-24 15:53:08 -0700

    attempt backward compatibility

---
+++ unstructured_ingest/v2/interfaces/file_data.py
@@ -0,0 +1,6 @@
+"""
+COMPATABILITY NOTICE:
+This file has moved to the v2/types/ module.
+The following line exists for backward compatibility.
+"""
+from unstructured_ingest.v2.types.file_data import *
