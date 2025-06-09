75a41155b6ceb1a22fc6c1c00c362a24c0c0d0b1
Paweł Kmiecik <pawel.kmiecik@deepsense.ai>
2025-06-09 12:47:36 +0200

    feat: added display name to sql and astra db connectors

--- unstructured_ingest/processes/connectors/astradb.py
+++ unstructured_ingest/processes/connectors/astradb.py
@@ -195,8 +195,10 @@ class AstraDBIndexer(Indexer):
         all_ids = self._get_doc_ids()
         ids = list(all_ids)
         id_batches = batch_generator(ids, self.index_config.batch_size)
-
         for batch in id_batches:
+            batch_items = [BatchItem(identifier=b) for b in batch]
+            display_name = (f"{self.index_config.collection_name}-{self.index_config.keyspace}"
+                            f"-[{batch_items[0].identifier}..{batch_items[-1].identifier}]")
             fd = AstraDBBatchFileData(
                 connector_type=CONNECTOR_TYPE,
                 metadata=FileDataSourceMetadata(
@@ -206,7 +208,8 @@ class AstraDBIndexer(Indexer):
                     collection_name=self.index_config.collection_name,
                     keyspace=self.index_config.keyspace,
                 ),
-                batch_items=[BatchItem(identifier=b) for b in batch],
+                batch_items=batch_items,
+                display_name=display_name,
             )
             yield fd
 
--- unstructured_ingest/processes/connectors/sql/sql.py
+++ unstructured_ingest/processes/connectors/sql/sql.py
@@ -130,7 +130,11 @@ class SQLIndexer(Indexer, ABC):
                 (len(ids) + self.index_config.batch_size - 1) // self.index_config.batch_size
             )
         ]
+
         for batch in id_batches:
+            batch_items = [BatchItem(identifier=str(b)) for b in batch]
+            display_name = (f"{self.index_config.table_name}-{self.index_config.id_column}"
+                            f"-[{batch_items[0].identifier}..{batch_items[-1].identifier}]")
             # Make sure the hash is always a positive number to create identified
             yield SqlBatchFileData(
                 connector_type=self.connector_type,
@@ -140,7 +144,8 @@ class SQLIndexer(Indexer, ABC):
                 additional_metadata=SqlAdditionalMetadata(
                     table_name=self.index_config.table_name, id_column=self.index_config.id_column
                 ),
-                batch_items=[BatchItem(identifier=str(b)) for b in batch],
+                batch_items=batch_items,
+                display_name=display_name
             )
 
 
