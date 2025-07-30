7f166512460e893a15512fc33482e71d30c3ffad
Filip Knefel <filip@unstructured.io>
2025-07-30 10:21:21 +0200

    Don't overwrite embeddings with each batch

--- unstructured_ingest/embed/interfaces.py
+++ unstructured_ingest/embed/interfaces.py
@@ -123,14 +123,14 @@ class AsyncBaseEmbeddingEncoder(BaseEncoder, ABC):
         elements = elements.copy()
         elements_with_text = [e for e in elements if e.get("text")]
         texts = [e["text"] for e in elements_with_text]
-        embeddings = []
+        all_embeddings = []
         try:
             for batch in batch_generator(texts, batch_size=self.config.batch_size or len(texts)):
-                embeddings = await self.embed_batch(client=client, batch=batch)
-                embeddings.extend(embeddings)
+                embeddings_batch = await self.embed_batch(client=client, batch=batch)
+                all_embeddings.extend(embeddings_batch)
         except Exception as e:
             raise self.wrap_error(e=e)
-        for element, embedding in zip(elements_with_text, embeddings):
+        for element, embedding in zip(elements_with_text, all_embeddings):
             element[EMBEDDINGS_KEY] = embedding
         return elements
 
