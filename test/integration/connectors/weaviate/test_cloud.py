a8fdcea542a201e1530fe73a7d15f10f24424afe
Bryan Chen <bryan.chen@unstructured.io>
2025-08-08 00:19:48 -0400

    fix integration test

--- test/integration/connectors/weaviate/test_cloud.py
+++ test/integration/connectors/weaviate/test_cloud.py
@@ -1,7 +1,7 @@
 import pytest
-from unstructured_ingest.errors_v2 import ValidationError
 
 from test.integration.connectors.utils.constants import DESTINATION_TAG, VECTOR_DB_TAG
+from unstructured_ingest.errors_v2 import ValueError
 from unstructured_ingest.processes.connectors.weaviate.cloud import (
     CONNECTOR_TYPE,
     CloudWeaviateAccessConfig,
@@ -11,7 +11,7 @@ from unstructured_ingest.processes.connectors.weaviate.cloud import (
 
 @pytest.mark.tags(CONNECTOR_TYPE, DESTINATION_TAG, VECTOR_DB_TAG)
 def test_weaviate_failing_connection_config():
-    with pytest.raises(ValidationError):
+    with pytest.raises(ValueError):
         CloudWeaviateConnectionConfig(
             access_config=CloudWeaviateAccessConfig(api_key="my key", password="password"),
             username="username",
