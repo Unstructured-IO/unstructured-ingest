13bfdfe98774ff3a0cc19cfaa84433f5150bf99d
David Potter <potterdavidm@gmail.com>
2026-03-02 15:54:18 -0800

    set the text field for auto created tables to unicode

--- unstructured_ingest/processes/connectors/assets/teradata_elements_schema.sql
+++ unstructured_ingest/processes/connectors/assets/teradata_elements_schema.sql
@@ -3,7 +3,7 @@ CREATE MULTISET TABLE "elements"
     "id"         VARCHAR(256) NOT NULL,
     "record_id"  VARCHAR(1024) NOT NULL,
     "element_id" VARCHAR(256) NOT NULL,
-    "text"       CLOB,
+    "text"       CLOB CHARACTER SET UNICODE,
     "type"       VARCHAR(256),
     "metadata"   JSON
 )
